7ba2346eaaedf9e7dfc011a6f782976f398ee2d5
Ian Miell <ian.miell@openbet.com>
2014-03-30 17:42:39 +0100

    more examples added

--- bin/create_skeleton.sh
+++ bin/create_skeleton.sh
@@ -74,9 +74,12 @@ cat >> ${DIR}/README.md << END
 #LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
+
+${MODULE_NAME}: description of module directory in here
 END
 touch ${DIR}/build.sh
 cat >> ${DIR}/build.sh << END
+#!/bin/bash
 #The MIT License (MIT)
 #
 #Copyright (C) 2014 OpenBet Limited
@@ -99,8 +102,9 @@ cat >> ${DIR}/build.sh << END
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
 
-${MODULE_NAME}: description of module directory in here
+python ${SHUTIT_DIR}/shutit_main.py
 END
+chmod +x ${DIR}/build.sh
 touch ${DIR}/resources/README.md
 cat >> ${DIR}/resources/README.md << END
 #The MIT License (MIT)
@@ -201,33 +205,6 @@ cat >> ${DIR}/configs/build.cnf << END
 [com.mycorp.${MODULE_NAME}]
 build:yes
 END
-# build.sh
-cat >> ${DIR}/build.sh << END
-#The MIT License (MIT)
-#
-#Copyright (C) 2014 OpenBet Limited
-#
-#Permission is hereby granted, free of charge, to any person obtaining a copy of
-#this software and associated documentation files (the "Software"), to deal in
-#the Software without restriction, including without limitation the rights to
-#use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-#of the Software, and to permit persons to whom the Software is furnished to do
-#so, subject to the following conditions:
-#
-#The above copyright notice and this permission notice shall be included in all
-#copies or substantial portions of the Software.
-#
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-#THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#SOFTWARE.
-
-python ${SHUTIT_DIR}/shutit_main.py
-END
-chmod +x ${DIR}/build.sh
 
 # Include bash script
 if [[ x${3} != "x" ]]
