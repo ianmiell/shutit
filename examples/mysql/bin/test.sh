8ba480690f334ca67eb656069a9b7eaa969ffa5c
Aidan Hobson Sayers <aidanhs@cantab.net>
2014-04-05 20:53:05 +0100

    Make these like the others so they actually work

--- examples/mysql/bin/test.sh
+++ examples/mysql/bin/test.sh
@@ -21,33 +21,20 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
 
-# Test the building of this module
 set -e
-if [[ $0 != test.sh ]] && [[ $0 != ./test.sh ]] && [[ $0 != create_skeleton.sh ]] && [[ $0 != ./create_skeleton.sh ]]
+if [ x\$1 = 'x' ]
 then
-        echo
-        echo "Called as: $0"
-	echo "Must be run from test dir like:"
-        echo
-        echo "  test.sh <path_to_shutit_dir>"
-        echo
-        echo "or"
-        echo
-        echo "  ./test.sh <path_to_shutit_dir>"
-        exit
-fi
-if [ x$1 = 'x' ]
-then
-	echo "Must supply path to core ShutIt directory"
-	exit 1
+        echo "Must supply path to shutit directory"
+        exit 1
 fi
 cd ..
 ./build.sh
 if [[ $? -eq 0 ]]
 then
-	cd -
-	exit 0
+        cd -
+        exit 0
 else
-	cd -
-	exit 1
+        cd -
+        exit 1
 fi
+
