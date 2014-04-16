a0e0407a1b51b7376618eed9ef1d107de6b48a8b
Ian Miell <ian.miell@gmail.com>
2014-04-16 22:35:09 +0100

    remove OB ref

--- examples/mysql/mysql.py
+++ examples/mysql/mysql.py
@@ -39,7 +39,6 @@ class mysql(ShutItModule):
 		util.handle_login(container_child,config_dict,'mysql_tmp_prompt')
 		expect = config_dict['expect_prompts']['mysql_tmp_prompt']
 		root_pass = config_dict['shutit.tk.mysql.mysql']['root_password']
-		# From the perfmon doc set the mysql root password non-interactively
 		util.send_and_expect(container_child,"apt-get update",expect,record_command=False)
 		util.send_and_expect(container_child,"""debconf-set-selections <<< 'mysql-server mysql-server/root_password password {0}'""".format(root_pass),expect,record_command=False)
 		util.send_and_expect(container_child,"""sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password {0}'""".format(root_pass),expect,record_command=False)
