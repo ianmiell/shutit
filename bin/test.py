38344c572127fb53e76350de2c7005bf00c24df3
Aidan Hobson Sayers <aidanhs@cantab.net>
2014-05-02 03:38:37 +0100

    Some more dep checking tests

--- bin/test.py
+++ bin/test.py
@@ -44,7 +44,7 @@ class TestShutItDepChecking(unittest.TestCase):
 		self.config_dict = shutit_global.config_dict = self._config_dict.copy()
 		recupdate(self.config_dict, {
 			'build': {
-				'tutorial': False, 'show_depgraph_only': False
+				'tutorial': False, 'debug': False, 'show_depgraph_only': False
 			},
 			'host': {'shutit_module_paths': 'dummy1:dummy2'}
 		})
@@ -60,6 +60,51 @@ class TestShutItDepChecking(unittest.TestCase):
 				depends_on=["tk.shutit.test0"])
 		}
 		shutit_id_list = ['tk.shutit.test1']
+		self.assertRaises(
+			ShutItTestException,
+			shutit_main.check_deps,
+			self.config_dict, shutit_map, shutit_id_list
+		)
+
+	def test_dep_build_err(self):
+		self.config_dict.update({
+			'tk.shutit.test1': {'build': False, 'build_ifneeded': False, 'remove': False},
+			'tk.shutit.test2': {'build': True, 'remove': False}
+		})
+		shutit_map = {
+			'tk.shutit.test2': Bunch(
+				module_id='tk.shutit.test2',
+				run_order=1.2,
+				depends_on=["tk.shutit.test1"]),
+			'tk.shutit.test1': Bunch(
+				module_id='tk.shutit.test1',
+				run_order=1.1,
+				depends_on=[],
+				is_installed=lambda c: False)
+		}
+		shutit_id_list = ['tk.shutit.test1', 'tk.shutit.test2']
+		self.assertRaises(
+			ShutItTestException,
+			shutit_main.check_deps,
+			self.config_dict, shutit_map, shutit_id_list
+		)
+
+	def test_dep_order_err(self):
+		self.config_dict.update({
+			'tk.shutit.test1': {'build': True, 'remove': False},
+			'tk.shutit.test2': {'build': True, 'remove': False}
+		})
+		shutit_map = {
+			'tk.shutit.test2': Bunch(
+				module_id='tk.shutit.test2',
+				run_order=1.2,
+				depends_on=["tk.shutit.test1"]),
+			'tk.shutit.test1': Bunch(
+				module_id='tk.shutit.test1',
+				run_order=1.9,
+				depends_on=[])
+		}
+		shutit_id_list = ['tk.shutit.test1', 'tk.shutit.test2']
 		self.assertRaises(
 			ShutItTestException,
 			shutit_main.check_deps,
