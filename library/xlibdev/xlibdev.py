f51ad237a994933a99065a1b4ee4116c8d551b9d
Aidan Hobson Sayers <aidanhs@cantab.net>
2014-05-10 19:07:47 +0100

    Remove some useless methods

--- library/xlibdev/xlibdev.py
+++ library/xlibdev/xlibdev.py
@@ -22,101 +22,15 @@ import util
 
 class xlibdev(ShutItModule):
 
-	# check_ready
-	# 
-	# Check whether we are ready to build this module.
-	# 
-	# This is called before the build, to ensure modules have 
-	# their requirements in place (eg files required to be mounted 
-	# in /resources). Checking whether the build will happen (and
-	# therefore whether the check should take place) will be 
-	# determined by the framework.
-	# 
-	# Should return True if it ready, else False.
-	def check_ready(self,shutit):
-		config_dict = shutit.cfg
-		return True
-
-	# is_installed
-	#
-	# Determines whether the module has been built in this container
-	# already.
-	#
-	# Should return True if it is certain it's there, else False.
 	def is_installed(self,shutit):
 		config_dict = shutit.cfg
 		return False
 
-	# build
-	#
-	# Run the build part of the module, which should ensure the module
-	# has been set up.
-	# If is_installed determines that the module is already there,
-	# this is not run.
-	#
-	# Should return True if it has succeeded in building, else False.
 	def build(self,shutit):
 		config_dict = shutit.cfg
 		container_child = util.get_pexpect_child('container_child') # Let's get the container child object from pexpect.
 		root_prompt_expect = config_dict['expect_prompts']['root_prompt'] # Set the string we expect to see once commands are done.
 		util.install(container_child,config_dict,'libx11-dev',config_dict['expect_prompts']['root_prompt'])
-		return True
-
-	# start
-	#
-	# Run when module should be installed (is_installed() or configured to build is true)
-	# Run after repo work.
-	def start(self,shutit):
-		config_dict = shutit.cfg
-		return True
-
-	# stop
-	#
-	# Run when module should be stopped.
-	# Run before repo work, and before finalize is called.
-	def stop(self,shutit):
-		config_dict = shutit.cfg
-		return True
-
-	# cleanup
-	#
-	# Cleanup the module, ie clear up stuff not needed for the rest of the build, eg tar files removed, apt-get cleans.
-	# Should return True if all is OK, else False.
-	# Note that this is only run if the build phase was actually run.
-	def cleanup(self,shutit):
-		config_dict = shutit.cfg
-		return True
-
-	# finalize
-	#
-	# Finalize the module, ie do things that need doing before we exit.
-	def finalize(self,shutit):
-		config_dict = shutit.cfg
-		return True
-
-	# remove
-	# 
-	# Remove the module, which should ensure the module has been deleted 
-	# from the system.
-	def remove(self,shutit):
-		config_dict = shutit.cfg
-		return True
-
-	# test
-	#
-	# Test the module is OK.
-	# Should return True if all is OK, else False.
-	# This is run regardless of whether the module is installed or not.
-	def test(self,shutit):
-		config_dict = shutit.cfg
-		return True
-
-	# get_config
-	#
-	# each object can handle config here
-	def get_config(self,shutit):
-		config_dict = shutit.cfg
-		cp = config_dict['config_parser']
 		return True
 
 
