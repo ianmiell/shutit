d36fa9d1e46f6ca542b2822f3d6d186ed72e86ba
Ian Miell <ian.miell@openbet.com>
2014-05-11 15:48:49 +0100

    adduser

---
+++ library/adduser/build_and_push.sh
@@ -0,0 +1,8 @@
+set -e
+python ..../shutit_main.py --config configs/push.cnf
+# Display config
+#python ../../shutit_main.py --sc
+# Debug
+#python ../../shutit_main.py --debug
+# Tutorial
+#python ../../shutit_main.py --tutorial
