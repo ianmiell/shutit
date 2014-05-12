eacb2fed31091536415f06d56652a242b1196eb3
Ian Miell <ian.miell@gmail.com>
2014-05-12 20:55:55 +0100

    moved handle_login and handle_revert_prompt into shutit. updated template

--- docs/shutit_module_template.py
+++ docs/shutit_module_template.py
@@ -1,5 +1,6 @@
 #Copyright (C) 2014 OpenBet Limited
 #
+#
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
 #in the Software without restriction, including without limitation the rights
@@ -17,6 +18,7 @@
 #IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 #CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+
 from shutit_module import ShutItModule
 import util
 
@@ -30,9 +32,8 @@ class template(ShutItModule):
 	#
 	# Should return True if it is certain it's there, else False.
 	def is_installed(self,shutit):
-		container_child = util.get_pexpect_child('container_child')
-		root_prompt_expect = shutit.cfg['expect_prompts']['root_prompt']
-		return util.file_exists(container_child,'/tmp/container_touched.sh',root_prompt_expect) and util.file_exists(container_child,'/tmp/README.md',root_prompt_expect)
+		return shutit.file_exists('/tmp/container_touched.sh') and shutit.file_exists('/tmp/README.md')
+
 
 	# build
 	#
@@ -43,71 +44,71 @@ class template(ShutItModule):
 	#
 	# Should return True if it has succeeded in building, else False.
 	def build(self,shutit):
-		container_child = util.get_pexpect_child('container_child') # Let's get the container child object from pexpect.
-		root_prompt_expect = shutit.cfg['expect_prompts']['root_prompt'] # Set the string we expect to see once commands are done.
+		shutit.set_default_expect(shutit.cfg['expect_prompts']['root_prompt'])
 		# Line number 49 should be the next one (so bash scripts can be inserted properly)
 
 		# DELETE THIS SECTION WHEN UNDERSTOOD - BEGIN
-		util.send_and_expect(container_child,'touch /tmp/deleteme',root_prompt_expect)
-		util.send_and_expect(container_child,'touch /tmp/container_touched.sh',root_prompt_expect)
-		util.add_line_to_file(container_child,'#This line should only appear once in the file','/tmp/container_touched.sh',root_prompt_expect)
-		util.add_line_to_file(container_child,'#This line should only appear once in the file','/tmp/container_touched.sh',root_prompt_expect)
+		shutit.send_and_expect('touch /tmp/deleteme')
+		shutit.send_and_expect('touch /tmp/container_touched.sh')
+		shutit.add_line_to_file('#This line should only appear once in the file','/tmp/container_touched.sh')
+		shutit.add_line_to_file('#This line should only appear once in the file','/tmp/container_touched.sh')
 		# We add the match_regexp because the string has a quote in it.
-		util.add_line_to_file(container_child,'echo "hello container"','/tmp/container_touched.sh',root_prompt_expect,match_regexp='echo .hello container.')
-		util.add_line_to_file(container_child,'sleep 10000000 &','/tmp/container_touched.sh',root_prompt_expect)
-		util.send_and_expect(container_child,'chmod +x /tmp/container_touched.sh',root_prompt_expect)
+		shutit.add_line_to_file('echo "hello container"','/tmp/container_touched.sh',match_regexp='echo .hello container.')
+		shutit.add_line_to_file('sleep 10000000 &','/tmp/container_touched.sh')
+		shutit.send_and_expect('chmod +x /tmp/container_touched.sh')
 		# Make sure passwd is installed
-		util.install(container_child,shutit.cfg,'passwd',root_prompt_expect)
+		shutit.install('passwd')
 		# Install mlocate
-		util.install(container_child,shutit.cfg,'mlocate',root_prompt_expect)
+		shutit.install('mlocate')
 		# Example of distro-specific password update
 		# record_command=False prevents the password being output at the end on debug. However, if it matches a password stored in config, 
 		# it will automatically redact it.
 		if shutit.cfg['container']['install_type'] == 'apt': # apt-based password update
 			# "check_exit" checks the exit code of the command you are running.
 			# We can't check the exit code as the command will not return when we see 'Retype new' on the terminal.
-			util.send_and_expect(container_child,'passwd','Enter new',check_exit=False)
-			util.send_and_expect(container_child,shutit.cfg['container']['password'],'Retype new',check_exit=False,record_command=False)
-			util.send_and_expect(container_child,shutit.cfg['container']['password'],root_prompt_expect)
+			shutit.send_and_expect('passwd','Enter new',check_exit=False)
+			shutit.send_and_expect(shutit.cfg['container']['password'],'Retype new',check_exit=False,record_command=False)
+			shutit.send_and_expect(shutit.cfg['container']['password'])
 		elif shutit.cfg['container']['install_type'] == 'yum': # yum-based password update
 			# Check_exit checks the exit code of the command you are running.
 			# We can't check the exit code as the command will not return when we see 'ew password' on the terminal.
-			util.send_and_expect(container_child,'passwd','ew password',check_exit=False)
-			util.send_and_expect(container_child,shutit.cfg['container']['password'],'ew password',check_exit=False,record_command=False)
-			util.send_and_expect(container_child,shutit.cfg['container']['password'],root_prompt_expect)
+			shutit.send_and_expect('passwd','ew password',check_exit=False)
+			shutit.send_and_expect(shutit.cfg['container']['password'],'ew password',check_exit=False,record_command=False)
+			shutit.send_and_expect(shutit.cfg['container']['password'])
 		# You can put multiple items you might expect in a list and handle accordingly:
-		res = util.send_and_expect(container_child,'sudo ls',['assword',root_prompt_expect],check_exit=False)
+		res = shutit.send_and_expect('sudo ls',['assword',shutit.cfg['expect_prompts']['root_prompt']],check_exit=False)
 		if res == 0:
-			util.send_and_expect(container_child,shutit.cfg['container']['password'],root_prompt_expect)
+			shutit.send_and_expect(shutit.cfg['container']['password'])
 		elif res == 1:
 			pass
 		else:
 			# We fail out on this case, as it's not expected.
 			util.fail('res: ' + str(res) + ' not handled')
 		# example of resource use (simple file, copy the README.md into the container)
-		util.send_and_expect(container_child,'cp /resources/README.md /tmp',root_prompt_expect)
+		shutit.send_and_expect('cp /resources/README.md /tmp')
 		# example of bespoke config use
 		if shutit.cfg[GLOBALLY_UNIQUE_STRING]['example_bool']:
-			util.add_line_to_file(container_child,'# ' + shutit.cfg[GLOBALLY_UNIQUE_STRING]['example'],'/tmp/container_touched.sh',root_prompt_expect)
+			shutit.add_line_to_file('# ' + shutit.cfg[GLOBALLY_UNIQUE_STRING]['example'],'/tmp/container_touched.sh')
 		# Example of login/logout handling
 		# When logging in, use the base prompt to attempt to match all prompts
 		# Note that we don't check_exit, because the exit value won't be meaningful.
-		util.send_and_expect(container_child,'su',shutit.cfg['expect_prompts']['base_prompt'],check_exit=False)
+		shutit.send_and_expect('su',shutit.cfg['expect_prompts']['base_prompt'],check_exit=False)
 		# Then call handle_login to set and get the bespoke prompt for the session
-		util.handle_login(container_child,shutit.cfg,'test_tmp_prompt')
-		util.send_and_expect(container_child,'echo "a command and some output"',shutit.cfg['expect_prompts']['test_tmp_prompt'])
+		shutit.handle_login('test_tmp_prompt')
+		shutit.set_default_expect(shutit.cfg['expect_prompts']['test_tmp_prompt'])
+		shutit.send_and_expect('echo "a command and some output"')
 		# We're about to exit, so handle the reversion of the prompt using the base_prompt again.
-		util.handle_revert_prompt(container_child,shutit.cfg['expect_prompts']['base_prompt'],'test_tmp_prompt')
-		util.send_and_expect(container_child,'exit',root_prompt_expect)
-
+		shutit.handle_revert_prompt(shutit.cfg['expect_prompts']['base_prompt'],'test_tmp_prompt')
+		shutit.set_default_expect(shutit.cfg['expect_prompts']['root_prompt'])
+		shutit.send_and_expect('exit')
 
 		# Example of temporary debugging
 		orig_debug_val = shutit.cfg['build']['debug']
 		shutit.cfg['build']['debug'] = True
-		util.send_and_expect(container_child,'echo "Command to look at debug output"',root_prompt_expect)
-		util.send_and_expect(container_child,'echo "Another command to look at debug output"',root_prompt_expect)
+		shutit.send_and_expect('echo "Command to look at debug output"')
+		shutit.send_and_expect('echo "Another command to look at debug output"')
 		shutit.cfg['build']['debug'] = orig_debug_val
-		util.pause_point(container_child,"""
+		shutit.pause_point("""
 				The doubling up of text is expected; try and ignore it :)
 				When you're done, "Ctrl" and "]" at the same time.
 				You should then see your inputted lines in the output.
@@ -144,9 +145,7 @@ class template(ShutItModule):
 	# Should return True if it ready, else False.
 	# OPTIONAL part of lifecycle - uncomment to include
 	#def check_ready(self,shutit):
-	#	container_child = util.get_pexpect_child('container_child')
-	#	root_prompt_expect = shutit.cfg['expect_prompts']['root_prompt']
-	#	return util.file_exists(container_child,'/resources/README.md',root_prompt_expect)
+	#	return shutit.file_exists('/resources/README.md')
 
 	# start
 	#
@@ -154,11 +153,9 @@ class template(ShutItModule):
 	# Run after repo work.
 	# OPTIONAL part of lifecycle - uncomment to include
 	#def start(self,shutit):
-	#	container_child = util.get_pexpect_child('container_child')
-	#	root_prompt_expect = shutit.cfg['expect_prompts']['root_prompt']
 	#	# example of starting something
-	#	util.send_and_expect(container_child,'cat /tmp/container_touched.sh',root_prompt_expect)
-	#	util.send_and_expect(container_child,'sh /tmp/container_touched.sh',root_prompt_expect)
+	#	shutit.send_and_expect('cat /tmp/container_touched.sh')
+	#	shutit.send_and_expect('sh /tmp/container_touched.sh')
 	#	return True
 
 	# stop
@@ -167,10 +164,8 @@ class template(ShutItModule):
 	# Run before repo work, and before finalize is called.
 	# OPTIONAL part of lifecycle - uncomment to include
 	#def stop(self,shutit):
-	#	container_child = util.get_pexpect_child('container_child')
-	#	root_prompt_expect = shutit.cfg['expect_prompts']['root_prompt']
 	#	# example of stopping something
-	#	util.send_and_expect(container_child,"""ps -ef | grep -v grep | grep container_touched.sh | awk '{print $1}' | sed 's/\([0-9][0-9]*\)/kill \\1/' | sh""",root_prompt_expect)
+	#	shutit.send_and_expect("""ps -ef | grep -v grep | grep container_touched.sh | awk '{print $1}' | sed 's/\([0-9][0-9]*\)/kill \\1/' | sh""")
 	#	return True
 
 	# cleanup
@@ -180,9 +175,7 @@ class template(ShutItModule):
 	# Note that this is only run if the build phase was actually run.
 	# OPTIONAL part of lifecycle - uncomment to include
 	#def cleanup(self,shutit):
-	#	container_child = util.get_pexpect_child('container_child')
-	#	root_prompt_expect = shutit.cfg['expect_prompts']['root_prompt']
-	#	util.send_and_expect(container_child,'rm -f /tmp/deleteme',root_prompt_expect)
+	#	shutit.send_and_expect('rm -f /tmp/deleteme')
 	#	return True
 
 	# finalize
@@ -190,10 +183,8 @@ class template(ShutItModule):
 	# Finalize the module, ie do things that need doing before we exit.
 	# OPTIONAL part of lifecycle - uncomment to include
 	#def finalize(self,shutit):
-	#	container_child = util.get_pexpect_child('container_child')
-	#	root_prompt_expect = shutit.cfg['expect_prompts']['root_prompt']
 	#	# Right at the end we want to ensure the locate db is up to date.
-	#	util.send_and_expect(container_child,'updatedb',root_prompt_expect)
+	#	shutit.send_and_expect('updatedb')
 	#	return True
 
 	# remove
@@ -202,10 +193,8 @@ class template(ShutItModule):
 	# from the system.
 	# OPTIONAL part of lifecycle - uncomment to include
 	#def remove(self,shutit):
-	#	container_child = util.get_pexpect_child('container_child')
-	#	root_prompt_expect = shutit.cfg['expect_prompts']['root_prompt']
-	#	util.send_and_expect(container_child,'rm -f /tmp/container_touched.sh',root_prompt_expect)
-	#	util.send_and_expect(container_child,'rm -f /tmp/README.md',root_prompt_expect)
+	#	shutit.send_and_expect('rm -f /tmp/container_touched.sh')
+	#	shutit.send_and_expect('rm -f /tmp/README.md')
 	#	# TODO: remove the installed apps DEPENDS on install tracking being available.
 	#	return True
 
@@ -216,10 +205,7 @@ class template(ShutItModule):
 	# This is run regardless of whether the module is installed or not.
 	# OPTIONAL part of lifecycle - uncomment to include
 	#def test(self,shutit):
-	#	container_child = util.get_pexpect_child('container_child')
-	#	root_prompt_expect = shutit.cfg['expect_prompts']['root_prompt']
-	#	# Check the packages we need are installed.
-	#	return util.package_installed(container_child,shutit.cfg,'mlocate',root_prompt_expect) and util.package_installed(container_child,shutit.cfg,'passwd',root_prompt_expect)
+	#	return shutit.package_installed('mlocate') and shutit.package_installed('passwd')
 
 
 
