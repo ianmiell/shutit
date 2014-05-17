e9c2da69c92a1ffc367998c4224f0dfe9ee20dce
Ian Miell <ian.miell@openbet.com>
2014-05-17 10:09:54 +0100

    skeleton updated

--- library/adduser/build_and_push.sh
+++ library/adduser/build_and_push.sh
@@ -1,8 +1,8 @@
 set -e
-python ..../shutit_main.py --config configs/push.cnf
+../../shutit build --config configs/push.cnf
 # Display config
-#python ../../shutit_main.py --sc
+#../../shutit sc
 # Debug
-#python ../../shutit_main.py --debug
+#../../shutit build --debug
 # Tutorial
-#python ../../shutit_main.py --tutorial
+#../../shutit build --tutorial
