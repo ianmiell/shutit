208755740f1b0c2ab8d411f0450bbaeb7c34a936
Ian Miell <ian.miell@gmail.com>
2014-05-21 20:48:00 +0100

    comments and echos

--- library/win2048/resources/start_win2048.sh
+++ library/win2048/resources/start_win2048.sh
@@ -29,11 +29,14 @@ sleep 5
 ## Example for automating single attempt repeatedly
 if [[ x$key != 'x' ]]
 then
+	echo "pressing $key"
 	#Hit key, eg Down for down arrow
 	xdotool key KP_${key}
 	#Wait for game over to appear
+	echo "Waiting for game over"
 	sleep 5
 	xdotool key KP_Page_Down
+	echo "Pushing page down key"
 	sleep 2
 	# Take a screenshot, which either has "try again on it, or doesn't"
 	scrot -q 100 -u /root/a.png
@@ -43,14 +46,15 @@ then
 	# 0 = match(es)
 	# 1 = no match
 	# else error
+	echo "Searching for 'try again' image"
 	visgrep -x 0 -y 0 -t 100000 /root/a.png /root/tryagain.pat /root/tryagain.pat
 	res=$?
 	if [[ $res = 1 ]]
 	then
-		echo OK
+		echo "OK - no 'try again' found"
 	else
-		echo FAIL
-	       exit 1
+		echo "FAIL - 'try again' seen"
+		exit 1
 	fi
 fi
 /bin/bash
