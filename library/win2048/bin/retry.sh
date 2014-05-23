670b3a646af0f739a08b5b4bec658d42edaa2dfb
Ian Miell <ian.miell@gmail.com>
2014-05-23 08:38:35 +0100

    sleep

--- library/win2048/bin/retry.sh
+++ library/win2048/bin/retry.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Input key, eg Right, Left, Up, Down, or blank for no key"
 read key
-while [ 1 ]; do sudo docker run -t -i -p 5902:5901 -p 6081:6080 -e key=$key -e HOME=/root imiell/2048 /root/start_vnc.sh; done
+while [ 1 ]; do sudo docker run -t -i -p 5902:5901 -p 6081:6080 -e key=$key -e HOME=/root imiell/2048 /root/start_vnc.sh; echo "CTRL-C now to exit"; sleep 2; done
