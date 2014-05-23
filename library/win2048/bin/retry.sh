c2eb5a86c657e986eeb770c0021adcf3aaf4544c
Ian Miell <ian.miell@openbet.com>
2014-05-23 09:08:58 +0100

    better

--- library/win2048/bin/retry.sh
+++ library/win2048/bin/retry.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 echo "Input key, eg Right, Left, Up, Down, or blank for no key"
 read key
-while [ 1 ]; do sudo docker run -t -i -p 5902:5901 -p 6081:6080 -e key=$key -e HOME=/root imiell/2048 /root/start_vnc.sh; echo "CTRL-C now to exit"; sleep 2; done
+echo "Input image, default is: $(whoami)/2048"
+read image
+if [[ $image = '' ]]
+	image=$(whoami)/2048
+while [ 1 ]; do sudo docker run -t -i -p 5902:5901 -p 6081:6080 -e key=$key -e HOME=/root $image /root/start_vnc.sh; echo "CTRL-C now to exit"; sleep 2; done
