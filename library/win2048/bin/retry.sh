9095e8f290264d82a925e3c04b89f5801473dcf6
Ian Miell <ian.miell@gmail.com>
2014-05-23 17:30:28 +0100

    server mod and keys

--- library/win2048/bin/retry.sh
+++ library/win2048/bin/retry.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-echo "Input key, eg Right, Left, Up, Down, or blank for no key"
-read key
+echo "Input keys, eg "Right Left Down Up", Left, Up, Down, or blank for no key"
+read keys
 echo "Input image, default is: $(whoami)/2048"
 read image
 if [[ $image = '' ]]
 then
 	image=$(whoami)/2048
 fi
-while [ 1 ]; do sudo docker run -t -i -p 5902:5901 -p 6081:6080 -e key=$key -e HOME=/root $image /root/start_vnc.sh; echo "CTRL-C now to exit"; sleep 2; done
+while [ 1 ]
+do
+	echo "Running with: $keys being pressed on image: $image"
+	sudo docker run -t -i -p 5902:5901 -p 6081:6080 -e keys="$keys" -e HOME=/root $image /root/start_vnc.sh
+	echo "CTRL-C now to exit"
+	sleep 2
+done
