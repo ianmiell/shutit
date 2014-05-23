25c5c5804d3ca0ab6b4f9e5159a8cce7bc14ac1f
Ian Miell <ian.miell@openbet.com>
2014-05-23 09:09:18 +0100

    sh, not py

--- library/win2048/bin/retry.sh
+++ library/win2048/bin/retry.sh
@@ -4,5 +4,7 @@ read key
 echo "Input image, default is: $(whoami)/2048"
 read image
 if [[ $image = '' ]]
+then
 	image=$(whoami)/2048
+fi
 while [ 1 ]; do sudo docker run -t -i -p 5902:5901 -p 6081:6080 -e key=$key -e HOME=/root $image /root/start_vnc.sh; echo "CTRL-C now to exit"; sleep 2; done
