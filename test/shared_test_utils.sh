f21ff6e6c6fe0eb4a115684f6b10c714da043401
Aidan Hobson Sayers <aidanhs@cantab.net>
2014-05-27 22:50:56 +0100

    Simplification, tab indents

--- test/shared_test_utils.sh
+++ test/shared_test_utils.sh
@@ -9,29 +9,29 @@ readonly SHUTIT_PARALLEL_BUILD
 
 set -o errexit
 set -o nounset
-#set -x 
+#set -x
 
 function set_shutit_options() {
-        local CNAME
-        CNAME=shutit_test_container_$(dd if=/dev/urandom bs=256 count=1 2>/dev/null | md5sum | awk '{print $1}')
-        export SHUTIT_OPTIONS="-s container name $CNAME"
+	local CNAME
+	CNAME=shutit_test_container_$(dd if=/dev/urandom bs=256 count=1 2>/dev/null | md5sum | awk '{print $1}')
+	export SHUTIT_OPTIONS="-s container name $CNAME"
 }
 
 function cleanup() {
-        CONTAINERS=$($DOCKER ps -a | grep shutit_test_container_ | awk '{print $1}')
-        if [[ "x${1:-}" = "xhard" ]]
+	CONTAINERS=$($DOCKER ps -a | grep shutit_test_container_ | awk '{print $1}')
+	if [[ "x${1:-}" = "xhard" ]]
 	then
-                $DOCKER kill $CONTAINERS >/dev/null 2>&1 || /bin/true
-        fi
-        $DOCKER rm $CONTAINERS >/dev/null 2>&1 || /bin/true
+		$DOCKER kill $CONTAINERS >/dev/null 2>&1 || /bin/true
+	fi
+	$DOCKER rm $CONTAINERS >/dev/null 2>&1 || /bin/true
 }
 
 function failure() {
-        echo "============================================"
-        echo "FAILED"
-        echo "$1"
-        echo "============================================"
-        cleanup hard
-        exit 1
+	echo "============================================"
+	echo "FAILED"
+	echo "$1"
+	echo "============================================"
+	cleanup hard
+	exit 1
 }
 
