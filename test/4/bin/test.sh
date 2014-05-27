5ba982332204f82f7f4466e58b0a4d767d277aaa
Aidan Hobson Sayers <aidanhs@cantab.net>
2014-05-27 21:32:16 +0100

    Don't refer to create_skeleton.sh any more

--- test/4/bin/test.sh
+++ test/4/bin/test.sh
@@ -9,19 +9,6 @@
 #THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 set -e
-if [[ ./create_skeleton.sh != test.sh ]] && [[ ./create_skeleton.sh != ./test.sh ]] && [[ ./create_skeleton.sh != create_skeleton.sh ]] && [[ ./create_skeleton.sh != ./create_skeleton.sh ]]
-then
-        echo
-        echo "Called as: ./create_skeleton.sh"
-	echo "Must be run from test dir like:"
-        echo
-        echo "  test.sh <path_to_shutit_dir>"
-        echo
-        echo "or"
-        echo
-        echo "  ./test.sh <path_to_shutit_dir>"
-        exit
-fi
 if [ x$1 = 'x' ]
 then
 	echo "Must supply path to shutit directory"
