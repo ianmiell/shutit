aa31c1fabecb7519f214aa82bf4fd202221e7836
Aidan Hobson Sayers <aidanhs@cantab.net>
2014-05-28 01:27:29 +0100

    First attempt at getting rid of test_build

--- library/docker/test_build.sh
+++
@@ -1,9 +0,0 @@
-# This file tests your build, removing the container when done.
-set -e
-../../shutit build -s container rm yes --shutit_module_path ../ssh_server
-# Display config
-#../../shutit sc
-# Debug
-#../../shutit build --debug
-# Tutorial
-#../../shutit build --tutorial
--- library/shutit/test.sh
+++ library/shutit/test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Test the building of this module
 set -e
-if [[ $0 != test.sh ]] && [[ $0 != ./test.sh ]]
+if [ $0 != test.sh ] && [ $0 != ./test.sh ]
 then
-        echo 
+        echo
         echo "Called as: $0"
-	echo "Must be run from test dir like:"
+        echo "Must be run from module root dir like:"
         echo
         echo "  test.sh <path_to_shutit_dir>"
         echo
@@ -14,15 +14,5 @@ then
         echo "  ./test.sh <path_to_shutit_dir>"
         exit
 fi
-if [ x$1 = 'x' ]
-then
-	echo "Must supply path to core ShutIt directory"
-	exit 1
-fi
-./test_build.sh
-if [[ $? -eq 0 ]]
-then
-	exit 0
-else
-	exit 1
-fi
+export SHUTIT_OPTIONS="$SHUTIT_OPTIONS -s container rm yes"
+./build.sh $1
