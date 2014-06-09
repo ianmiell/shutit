3e17dcfd12fb83eee9e512ffea0b9fc7e6a67bc0
Aidan Hobson Sayers <aidanhs@cantab.net>
2014-06-09 15:55:38 +0100

    This is a single package manager install unused by anything

--- library/xlibdev/build_and_push.sh
+++
@@ -1,8 +0,0 @@
-set -e
-../../shutit build --config configs/push.cnf --image_tag imiell/vnc -m ../vnc
-# Display config
-#../../shutit sc
-# Debug
-#../../shutit build --debug
-# Tutorial
-#../../shutit build --tutorial
