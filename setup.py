b4ebfa7afc890bb04ed9199054b5e766b9e44c94
Ian Miell <ian.miell@gmail.com>
2014-06-13 07:27:54 +0100

    initial

--- setup.py
+++ setup.py
@@ -167,18 +167,20 @@ class conn_docker(ShutItModule):
 		shutit.log('\n\nCommand being run is:\n\n' + ' '.join(docker_command),force_stdout=True,prefix=False)
 		shutit.log('\n\nThis may download the image, please be patient\n\n',force_stdout=True,prefix=False)
 		container_child = pexpect.spawn(docker_command[0], docker_command[1:])
-		expect = ['assword',cfg['expect_prompts']['base_prompt'].strip(),'Waiting','ulling','endpoint','Download']
-		res = container_child.expect(expect,9999)
-		while True:
-			print res
-			if res == 0:
-				res = shutit.send_and_expect(cfg['host']['password'],child=container_child,expect=expect,timeout=9999,check_exit=False,fail_on_empty_before=False)
-			elif res == 1:
-				break
-			else:
-				print container_child.before + container_child.after
-				res = container_child.expect(expect,9999)
-				continue
+                if container_child.expect(['assword',cfg['expect_prompts']['base_prompt'].strip()],9999) == 0:
+                        shutit.send_and_expect(cfg['host']['password'],child=container_child,
+                                expect=cfg['expect_prompts']['base_prompt'],timeout=9999,check_exit=False)
+		#expect = ['assword',cfg['expect_prompts']['base_prompt'].strip(),'Waiting','ulling','endpoint','Download']
+		#res = container_child.expect(expect,9999)
+		#while True:
+		#	if res == 0:
+		#		res = shutit.send_and_expect(cfg['host']['password'],child=container_child,expect=expect,timeout=9999,check_exit=False,fail_on_empty_before=False)
+		#	elif res == 1:
+		#		break
+		#	else:
+		#		print container_child.before + container_child.after
+		#		res = container_child.expect(expect,9999)
+		#		continue
 		# Get the cid
 		time.sleep(1) # cidfile creation is sometimes slow...
 		cid = open(cfg['build']['cidfile']).read()
