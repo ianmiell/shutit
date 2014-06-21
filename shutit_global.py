e6f82b671790d87a0e6d54791d2119db6bd990f4
Ian Miell <ian.miell@gmail.com>
2014-06-21 15:07:43 +0100

    action_on_ret_code removed

--- shutit_global.py
+++ shutit_global.py
@@ -264,10 +264,10 @@ class ShutIt(object):
 				res = str(res)
 			self.log('child.after: \n' + child.after + '\n')
 			self.log('Exit value from command+\n' + send + '\nwas:\n' + res)
-			msg = '\nWARNING: command:\n' + send + '\nreturned unaccepted exit code: ' + res + '\nIf this is expected, pass in check_exit=False or an exit_values array into the send_and_expect function call.\nIf you want to error on these errors, set the config:\n[build]\naction_on_ret_code:error'
+			msg = '\nWARNING: command:\n' + send + '\nreturned unaccepted exit code: ' + res + '\nIf this is expected, pass in check_exit=False or an exit_values array into the send_and_expect function call.'
 			cfg['build']['report'] = cfg['build']['report'] + msg
-			if cfg['build']['action_on_ret_code'] == 'error':
-				shutit.fail(msg + '\n\nPause point on exit_code != 0. CTRL-C to quit',child=child)
+			if cfg['build']['interactive'] >= 1:
+				shutit.fail(msg + '\n\nPause point on exit_code != 0 (' + res + '). CTRL-C to quit',child=child)
 				#raise Exception('Exit value from command\n' + send + '\nwas:\n' + res)
 
 	def run_script(self,script,expect=None,child=None,in_shell=True):
