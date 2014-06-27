b1ca9c6e0ba915465b1476fdc63ccef6a3339a64
Ian Miell <ian.miell@openbet.com>
2014-06-27 17:54:33 +0100

    Record more info about the build, and sha512 the password 27 times. Some changes to bring the docker-registry up to date.

--- library/docker_registry/run.sh
+++ library/docker_registry/run.sh
@@ -1,2 +1,2 @@
 # Example for running
-docker.io run -t -i -v /srv/registry:/srv/registry -p 5000:5000 imiell/registry /docker-registry/run.sh
+docker.io run -t -i -v /your/host/registrydir:/srv/registry -p 5000:5000 imiell/registry /docker-registry/run.sh
