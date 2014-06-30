9df83e1fb217e03f3b97ee9b84c001343ce6ac7f
Ian Miell <ian.miell@gmail.com>
2014-06-30 22:54:13 +0100

    merged

--- shutit_main.py
+++ shutit_main.py
@@ -381,7 +381,7 @@ def build_module(shutit, module):
 	cfg['build']['report'] = cfg['build']['report'] + '\nCompleted module: ' + module.module_id
 	if cfg[module.module_id]['do_repository_work'] or cfg['build']['interactive'] >= 3:
 		shutit.log(util.build_report(shutit,'#Module:' + module.module_id),code='31')
-	if not cfg[module.module_id]['do_repository_work'] and cfg['build']['interactive'] >= 3:
+	if not cfg[module.module_id]['do_repository_work'] and cfg['build']['interactive'] >= 2:
 		shutit.log("\n\nDo you want to save state now we\'re at the end of this module? (" + module.module_id + ") (input y/n)",force_stdout=True)
 		cfg[module.module_id]['do_repository_work'] = (raw_input('') == 'y')
 	if cfg[module.module_id]['do_repository_work']:
@@ -391,7 +391,7 @@ def build_module(shutit, module):
 		shutit.do_repository_work(str(module.module_id) + '_' + str(module.run_order),password=cfg['host']['password'],docker_executable=cfg['host']['docker_executable'],force=True)
 		# Start all after we tag to ensure services are up as expected.
 		start_all(shutit, module.run_order)
-	if cfg['build']['interactive'] >= 3:
+	if cfg['build']['interactive'] >= 2:
 		shutit.log("\n\nDo you want to stop interactive mode? (input y/n)\n",force_stdout=True)
 		if raw_input('') == 'y':
 			cfg['build']['interactive'] = 0
