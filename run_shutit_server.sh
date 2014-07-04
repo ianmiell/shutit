2e43e00c926513edb681fe8cb2f0253205547323
Ian Miell <ian.miell@gmail.com>
2014-07-04 22:43:58 +0100

    numbers

--- run_shutit_server.sh
+++ run_shutit_server.sh
@@ -1,2 +1,2 @@
 #export SHUTIT_PORT=9080
-while [ 1 ]; do ./shutit serve -m library --debug 2>&1 > ~/shutit_server.log; sleep 60; done
+while [ 1 ]; do ./shutit serve -m library --debug 2>&1 > ~/shutit_server.log; sleep 2; done
