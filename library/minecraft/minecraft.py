2a7155c39dea6c54f100db865ccdf514d0dba70b
Ian Miell <ian.miell@gmail.com>
2014-07-05 19:29:21 +0100

    minecraft added, doesn't work out of the box

---
+++ library/minecraft/minecraft.py
@@ -0,0 +1,40 @@
+
+# Created from dockerfile: https://raw.githubusercontent.com/crosbymichael/Dockerfiles/master/minecraft/Dockerfile
+from shutit_module import ShutItModule
+
+class minecraft(ShutItModule):
+
+        def is_installed(self,shutit):
+                return False
+
+        def build(self,shutit):
+		shutit.send('echo "deb http://archive.ubuntu.com/ubuntu precise main universe" > /etc/apt/sources.list')
+		shutit.send('apt-get update')
+		shutit.send('apt-get install -y python-software-properties')
+		shutit.send('add-apt-repository ppa:webupd8team/java -y')
+		shutit.send('apt-get update')
+		shutit.send('echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections')
+		shutit.send('apt-get install -y oracle-java7-installer wget')
+		shutit.send('mkdir /minecraft')
+		shutit.send('wget -O /minecraft/minecraft.jar https://s3.amazonaws.com/Minecraft.Download/versions/1.7.4/minecraft_server.1.7.4.jar')
+		shutit.send('pushd /minecraft')
+		shutit.send('popd')
+                return True
+
+	def finalize(self,shutit):
+		return True
+
+	def test(self,shutit):
+		return True
+
+	def is_installed(self,shutit):
+		return False
+
+	def get_config(self,shutit):
+		return True
+
+def module():
+        return minecraft(
+                'shutit.tk.minecraft.minecraft', 782914092.00,
+                depends=['shutit.tk.setup']
+        )
---
+++ library/minecraft/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Test the building of this module
+set -e
+if [ $0 != test.sh ] && [ $0 != ./test.sh ]
+then
+	echo
+	echo "Called as: $0"
+	echo "Must be run from module root dir like:"
+	echo
+	echo "  test.sh <path_to_shutit_dir>"
+	echo
+	echo "or"
+	echo
+	echo "  ./test.sh <path_to_shutit_dir>"
+	exit
+fi
+./build.sh $1
