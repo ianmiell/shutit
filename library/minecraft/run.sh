7c93c62dc8b162aa4ca909609ca073acd8ec8c03
Ian Miell <ian.miell@gmail.com>
2014-07-06 18:45:53 +0100

    minecraft

--- library/minecraft/run.sh
+++ library/minecraft/run.sh
@@ -1,2 +1,2 @@
 # Example for running
-docker run -t -i -p 25565:25565 -v /:/ -v m:m -v i:i -v n:n -v e:e -v c:c -v r:r -v a:a -v f:f -v t:t minecraft java -jar minecraft.jar nogui -Xmx1024M -Xms768M
+docker run -t -i -p 25565:25565 -v /minecraft:/minecraft minecraft java -jar minecraft.jar nogui -Xmx1024M -Xms768M
