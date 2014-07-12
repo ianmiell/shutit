6c2bcb1561fa7a9099d3b92850e1c53f89ffdb93
Ian Miell <ian.miell@openbet.com>
2014-07-12 10:58:07 +0100

    distinguish between local on skeleton and local on build

--- run_shutit_server.sh
+++ run_shutit_server.sh
@@ -1,2 +1,7 @@
-#export SHUTIT_PORT=9080
+ARG=$1
+export SHUTIT_PORT=${ARG:-8080}
 while [ 1 ]; do ./shutit serve -m library --debug 2>&1 > ~/shutit_server.log; sleep 2; done
+
+#Handy
+#for p in $(jot 10 8081); do echo SHUTIT_PORT=$p ./shutit serve -m library \| tee /tmp/shutit_out_$p \&;  done | sh
+
