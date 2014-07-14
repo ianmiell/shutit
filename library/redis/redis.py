fcb20af601c5966a455a5de9d6ad031ff24e54d5
Ian Miell <ian.miell@openbet.com>
2014-07-14 23:23:56 +0100

    redis

---
+++ library/redis/redis.py
@@ -0,0 +1,42 @@
+
+# Created from dockerfile: /space/git/dockerfiles_repos/redis/Dockerfile
+# Maintainer:              
+from shutit_module import ShutItModule
+
+class redis(ShutItModule):
+
+        def is_installed(self,shutit):
+                return False
+
+        def build(self,shutit):
+		#add
+		shutit.send('groupadd -r redis && useradd -r -g redis redis')
+		shutit.send('apt-get update && apt-get install -y build-essential tcl valgrind')
+		shutit.send_host_dir('/usr/src/redis','context/.')
+		shutit.send('make -C /usr/src/redis')
+		#in
+		shutit.send('make -C /usr/src/redis test || true')
+		shutit.send('make -C /usr/src/redis install')
+		shutit.send('mkdir /data && chown redis:redis /data')
+		shutit.send('pushd /data')
+		shutit.send('popd')
+                return True
+
+	def finalize(self,shutit):
+		return True
+
+	def test(self,shutit):
+		return True
+
+	def is_installed(self,shutit):
+		return False
+
+	def get_config(self,shutit):
+		return True
+
+def module():
+        return redis(
+                'shutit.tk.redis.redis', 782914092.00,
+		description='',
+                depends=['shutit.tk.setup']
+        )
---
+++ library/redis/run.sh
@@ -0,0 +1,2 @@
+# Example for running
+docker run -t -i -p 6379:6379 -v /data:/data redis  redis-server
