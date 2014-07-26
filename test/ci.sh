3d2b34480bdaaf9efb01d94105b31a0859549c31
Ian Miell <ian.miell@openbet.com>
2014-07-26 20:32:46 +0100

    no force

--- test/ci.sh
+++ test/ci.sh
@@ -2,17 +2,18 @@
 
 # Simple CI for ShutIt
 
-FORCE=1
+FORCE=0
 SHUTIT_BUILD_DIR="/tmp/shutit_builddir"
+mkdir -p $SHUTIT_BUILD_DIR
 LOCKFILE="${SHUTIT_BUILD_DIR}/shutitci.lck"
 LOGFILE="${SHUTIT_BUILD_DIR}/shutit_build_${RANDOM}.log"
 if [[ -a $LOCKFILE ]]
 then
 	echo "Already running"
 	exit 
+else
+	touch $LOCKFILE
 fi
-
-touch $LOCKFILE
 
 git fetch origin master
 # See if there are any incoming changes
@@ -21,11 +22,10 @@ git log HEAD..origin/master --oneline
 if [[ $updates -gt 0 ]] || [[ $FORCE -gt 0 ]]
 then
 	git pull origin master
-	mkdir -p $SHUTIT_BUILD_DIR
 	./test.sh > $LOGFILE || EXIT_CODE=$?
         if [[ $EXIT_CODE -ne 0 ]]
 	then
-		cat $LOGFILE | mail -s "ANGRY SHUTIT" ian.miell@gmail.com
+		tail -100 $LOGFILE | mail -s "ANGRY SHUTIT" ian.miell@gmail.com
 	else
 		echo OK | mail -s "HAPPY SHUTIT" ian.miell@gmail.com
 	fi
