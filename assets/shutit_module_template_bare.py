3208f2a8d225c29e5ca2dcc4fdc9e0ee1e65338b
Ian Miell <ian.miell@gmail.com>
2014-08-03 12:52:05 +0100

    change default skeleton to add useful functions, and add maintainer field

--- assets/shutit_module_template_bare.py
+++ assets/shutit_module_template_bare.py
@@ -30,10 +30,32 @@ class template(ShutItModule):
     def build(self,shutit):
         return True
 
+    #def get_config(self,shutit):
+    #    return True
+
+    #def check_ready(self,shutit):
+    #    return True
+    
+    #def start(self,shutit):
+    #    return True
+
+    #def stop(self,shutit):
+    #    return True
+
+    #def finalize(self,shutit):
+    #    return True
+
+    #def remove(self,shutit):
+    #    return True
+
+    #def test(self,shutit):
+    #    return True
+
 def module():
     return template(
         GLOBALLY_UNIQUE_STRING, FLOAT,
         description='',
+        maintainer='',
         depends=['shutit.tk.setup']
     )
 
