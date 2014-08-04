bd0ba1b39cb3d82e32f6905daa9013b88791dd6b
Ian Miell <ian.miell@gmail.com>
2014-08-04 09:56:45 +0100

    puppet

--- library/puppet/puppet.py
+++ library/puppet/puppet.py
@@ -1,4 +1,5 @@
-
+"""puppet module from Pro Puppet book.
+"""
 # Created from dockerfile: /space/git/dockerfiles_repos/puppet/Dockerfile
 # Maintainer:              James Turnbull "james@lovedthanlost.net"
 from shutit_module import ShutItModule
@@ -10,7 +11,24 @@ class puppet(ShutItModule):
 
     def build(self, shutit):
         shutit.install('puppet')
+        shutit.install('puppetmaster') # Only for master
         shutit.install('librarian-puppet')
+        shutit.install('ruby')
+        shutit.install('libshadow-ruby1.8')
+        shutit.install('facter')
+        if 'puppet.conf' not in shutit.ls('/etc/puppet'):
+        	shutit.send('puppetmasterd --genconfig > puppet.conf')
+        # TODO: add:
+	#[master]
+	#certname=localhost
+
+        #
+        shutit.send('mkdir -p /etc/puppet/manifests')
+        shutit.send('touch /etc/puppet/manifests/site.pp')
+
+        # TODO: p 14 
+        # service puppetmaster start (non-debian)
+        # invoke-rc.d puppetmaster start (debian)
         return True
 
     def finalize(self, shutit):
@@ -29,5 +47,5 @@ def module():
     return puppet(
         'shutit.tk.puppet.puppet', 0.012412487,
         description='Puppet reference build',
-        depends=['shutit.tk.setup']
+        depends=['shutit.tk.setup','shutit.tk.ssh_keys']
     )
