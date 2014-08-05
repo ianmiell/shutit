8f9fe50a9430cf8c11b1db461f70ccb10875068a
Ian Miell <ian.miell@openbet.com>
2014-08-05 17:55:40 +0100

    add phusion/baseimage

--- test/shared_test_utils.sh
+++ test/shared_test_utils.sh
@@ -21,7 +21,7 @@ readonly SHUTIT
 SHUTIT_TEST_REPORT=""
 
 # Default distros
-SHUTITTEST_DISTROS=${SHUTITTEST_DISTROS:-ubuntu:12.04 ubuntu:14.04 debian:7.5 fedora:20}
+SHUTITTEST_DISTROS=${SHUTITTEST_DISTROS:-phusion/baseimage ubuntu:12.04 ubuntu:14.04 debian:7.5 fedora:20}
 
 set -o errexit
 set -o nounset
