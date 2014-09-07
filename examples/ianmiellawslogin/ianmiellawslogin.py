e832296b753b3aa4c07a0ecd8a8229782e74cb4c
Ian Miell <ian.miell@gmail.com>
2014-09-07 20:07:43 +0100

    latest

--- examples/ianmiellawslogin/ianmiellawslogin.py
+++ examples/ianmiellawslogin/ianmiellawslogin.py
@@ -15,30 +15,29 @@ class ianmiellawslogin(ShutItModule):
         if len(json_dict['Reservations']) != 0:
             shutit.fail('Cannot continue - you have reservations running already')
      
-        shutit.send('aws ec2 run-instances --image-id ' + shutit.cfg[self.module_id]['image_id'] + ' --instance-type ' + shutit.cfg[self.module_id]['instance_type'] + ' --key-name imiell_aws_eu')
+        shutit.send('aws ec2 run-instances --image-id ' + shutit.cfg[self.module_id]['image_id'] + ' --instance-type ' + shutit.cfg[self.module_id]['instance_type'] + ' --key-name imiell_aws_eu --security-groups launch-wizard-1')
         json_dict = json.loads(shutit.get_output())
         shutit.cfg[self.module_id]['instance_id'] = json_dict['Instances'][0]['InstanceId']
 
-        #ec2-authorize = see http://docs.aws.amazon.com/AWSEC2/latest/CommandLineReference/ApiReference-cmd-AuthorizeSecurityGroupIngress.html
-        
-        shutit.pause_point('aws ec2 describe-instances --filter Name=instance-state-name,Values=running')
-        #shutit.send('sleep 500')
+        # TODO - check with describe rather than pause
+        shutit.send('sleep 500')
         shutit.send('aws ec2 describe-instances --filter Name=instance-state-name,Values=running')
         json_dict = json.loads(shutit.get_output())
         shutit.cfg[self.module_id]['ec2_ip'] = json_dict['Reservations'][0]['Instances'][0]['PublicIpAddress']
 
         if shutit.send('ssh -i imiell_aws_eu.pem ec2-user@' + shutit.cfg[self.module_id]['ec2_ip'],expect=['continue connecting',shutit.cfg['expect_prompts']['base_prompt']],check_exit=False) == 0:
-        	shutit.send('yes',check_exit=False)
-        #shutit.send('sudo yum install docker',check_exit=False)
-        #shutit.set_default_expect()
-        #shutit.send('exit')
+       	    shutit.send('yes',check_exit=False)
+        shutit.send('sudo yum install docker',check_exit=False)
+        shutit.set_default_expect()
+        shutit.pause_point('docker pull something')
+        shutit.send('exit')
         return True
 
     def get_config(self,shutit):
         shutit.get_config(self.module_id, 'image_id', 'ami-672ce210')
         shutit.get_config(self.module_id, 'instance_type', 't1.micro')
         if shutit.cfg[self.module_id]['image_id'] == '':
-		    return False
+            return False
         return True
 
     def finalize(self, shutit):
