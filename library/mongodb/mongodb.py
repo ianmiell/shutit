c92acd222cbed8d89c90e5229750921167a4b7b4
Ian Miell <ian.miell@gmail.com>
2014-09-18 20:48:41 +0100

    remove apt-utils as presently it causes issues with 12.04

--- library/mongodb/mongodb.py
+++ library/mongodb/mongodb.py
@@ -11,7 +11,6 @@ class mongodb(ShutItModule):
         shutit.send('apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10')
         shutit.send('echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" | tee -a /etc/apt/sources.list.d/10gen.list')
         shutit.send('apt-get update')
-        shutit.send('apt-get -y install apt-utils')
         shutit.send('apt-get -y install mongodb-10gen')
         return True
 
