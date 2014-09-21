197f4038f6126be3218c252a416e0fb266723bcc
Ian Miell <ian.miell@gmail.com>
2014-09-22 00:48:20 +0100

    cidfiles and repository work removed where note relevant. some simplification

--- setup.py
+++ setup.py
@@ -408,21 +408,8 @@ class ConnBash(ShutItConnModule):
 		self._add_end_build_info(shutit)
 		# Finish with the container
 		shutit.pexpect_children['container_child'].sendline('exit')
-
-		cfg = shutit.cfg
-		host_child = shutit.pexpect_children['host_child']
-		shutit.set_default_child(host_child)
-		shutit.set_default_expect(cfg['expect_prompts']['real_user_prompt'])
-		# Tag and push etc
-		shutit.pause_point('\nDoing final committing/tagging on the overall \
-			container and creating the artifact.', \
-			child=shutit.pexpect_children['host_child'], \
-			print_input=False, level=3)
-		shutit.do_repository_work(cfg['repository']['name'], \
-			docker_executable=cfg['host']['docker_executable'], \
-			password=cfg['host']['password'])
-		# Final exits
-		host_child.sendline('rm -f ' + cfg['build']['cidfile']) # Exit raw bash
+		# Finish with the host
+		shutit.set_default_child(shutit.pexpect_children['host_child'])
 		host_child.sendline('exit') # Exit raw bash
 		return True
 
@@ -448,43 +435,33 @@ class ConnSSH(ShutItConnModule):
 	def build(self, shutit):
 		"""Sets up the machine ready for building.
 		"""
-
 		cfg = shutit.cfg
-
 		ssh_host = cfg[self.module_id]['ssh_host']
 		ssh_port = cfg[self.module_id]['ssh_port']
 		ssh_user = cfg[self.module_id]['ssh_user']
 		ssh_pass = cfg[self.module_id]['password']
 		ssh_key  = cfg[self.module_id]['ssh_key']
 		ssh_cmd  = cfg[self.module_id]['ssh_cmd']
-
 		opts = [
 			'-t',
 			'-o', 'UserKnownHostsFile=/dev/null',
 			'-o', 'StrictHostKeyChecking=no'
 		]
-
 		if ssh_pass == '':
 			opts += ['-o', 'PasswordAuthentication=no']
-
 		if ssh_port != '':
 			opts += ['-p', ssh_port]
-
 		if ssh_key != '':
 			opts += ['-i', ssh_key]
-
 		host_arg = ssh_host
 		if host_arg == '':
 			shutit.fail('No host specified for sshing')
 		if ssh_user != '':
 			host_arg = ssh_user + '@' + host_arg
-
 		cmd_arg = ssh_cmd
 		if cmd_arg == '':
 			cmd_arg = 'sudo su -s /bin/bash -'
-
 		ssh_command = ['ssh'] + opts + [host_arg, cmd_arg]
-
 		if cfg['build']['interactive'] >= 3:
 			print('\n\nAbout to connect to host.' +
 				'\n\n' + util.colour('31', '\n[Hit return to continue]'))
@@ -492,7 +469,6 @@ class ConnSSH(ShutItConnModule):
 		shutit.cfg['build']['ssh_command'] = ' '.join(ssh_command)
 		shutit.log('\n\nCommand being run is:\n\n' + shutit.cfg['build']['ssh_command'],
 			force_stdout=True, prefix=False)
-
 		container_child = pexpect.spawn(ssh_command[0], ssh_command[1:])
 		expect = ['assword', cfg['expect_prompts']['base_prompt'].strip()]
 		res = container_child.expect(expect, 10)
@@ -507,10 +483,8 @@ class ConnSSH(ShutItConnModule):
 			elif res == 1:
 				shutit.log('Prompt found, breaking out')
 				break
-
 		self._setup_prompts(shutit, container_child)
 		self._add_begin_build_info(shutit, ssh_command)
-
 		return True
 
 	def finalize(self, shutit):
@@ -520,10 +494,8 @@ class ConnSSH(ShutItConnModule):
 		self._add_end_build_info(shutit)
 		# Finish with the container
 		shutit.pexpect_children['container_child'].sendline('exit')
-
-		host_child = shutit.pexpect_children['host_child']
-		shutit.set_default_child(host_child)
-		shutit.set_default_expect(shutit.cfg['expect_prompts']['real_user_prompt'])
+		# Finish with the host
+		shutit.set_default_child(shutit.pexpect_children['host_child'])
 		# Final exits
 		host_child.sendline('exit') # Exit raw bash
 		return True
