c331d519fbc934a41be29f154b2a85c395f80fd8
Ian Miell <ian.miell@openbet.com>
2014-09-24 07:35:30 +0100

    interactive off

--- test/shared_test_utils.sh
+++ test/shared_test_utils.sh
@@ -37,9 +37,9 @@ function set_shutit_options() {
 	OPTS=${1:-none}
 	if [[ "$OPTS" = "none" ]]
 	then
-		export SHUTIT_OPTIONS="-s container name $CNAME -s repository tag no"
+		export SHUTIT_OPTIONS="-s container name $CNAME -s repository tag no --interactive 0"
 	else
-		export SHUTIT_OPTIONS="-s container name $CNAME $OPTS -s repository tag no"
+		export SHUTIT_OPTIONS="-s container name $CNAME $OPTS -s repository tag no --interactive 0"
 	fi
 }
 
