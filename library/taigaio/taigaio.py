eb28620fca2879ca234dab0ca8375fc4339e4fff
Ian Miell <ian.miell@openbet.com>
2014-10-05 16:03:12 +0100

    exit_value

--- library/taigaio/taigaio.py
+++ library/taigaio/taigaio.py
@@ -54,7 +54,7 @@ class taigaio(ShutItModule):
 		shutit.send('python manage.py runserver 0.0.0.0:8000 > /dev/null 2>&1 &')
 		# Newline required to make the expect work
 		shutit.send('''curl -w '\n' localhost:8000''')
-		shutit.send('kill %1')
+		shutit.send('kill %1',exit_values=[0,1])
 		return True
 
 def module():
