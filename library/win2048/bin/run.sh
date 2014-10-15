c5ab92983ab23048bb577f0dca7501cb25753b52
Ian Miell <ian.miell@gmail.com>
2014-10-15 18:26:57 +0100

    simpler

--- library/win2048/bin/run.sh
+++ library/win2048/bin/run.sh
@@ -1,3 +1,3 @@
 # Example for running
-docker run -d -p 5901:5901 -p 6080:6080 -e HOME=/root win2048 /root/start_win2048.sh
-# Now connect with vncviewer localhost:1
+docker run -d -p 5901:5901 -p 6080:6080 imiell/win2048 /root/start_win2048.sh
+echo "Now connect with vncviewer localhost:1"
