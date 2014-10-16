8c1e73dffdb0aacef25cc18b567cd9a8c76a5cdb
Ian Miell <ian.miell@openbet.com>
2014-10-16 19:54:21 +0100

    latest

--- library/binspector/binspector.py
+++ library/binspector/binspector.py
@@ -6,21 +6,24 @@ from shutit_module import ShutItModule
 
 class binspector(ShutItModule):
 
-
 	def is_installed(self, shutit):
 		return False
-
 
 	def build(self, shutit):
 		shutit.install('curl')
 		shutit.install('git')
+		shutit.send('pushd /opt')
 		shutit.send('git clone https://github.com/binspector/binspector.git')
-		shutit.send('./binspector/smoke_test.sh')
+		shutit.send('pushd /opt')
+		shutit.send('pushd /opt/binspector')
+		shutit.send('./configure.sh')
+		shutit.send('./build.sh')
+		shutit.send('./smoke_test.sh')
 		return True
 
 def module():
 	return binspector(
-		'shutit.tk.binspector.binspector', 0.132235246,
+		'shutit.tk.binspector.binspector', 0.3132235246,
 		description='binary inspector',
 		maintainer='ian.miell@gmail.com',
 		depends=['shutit.tk.setup','shutit.tk.llvm.llvm']
