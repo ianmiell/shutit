480bac00b7247e086e19d2655386510e4a17423a
Ian Miell <ian.miell@openbet.com>
2014-10-16 23:37:36 +0100

    latest

--- library/binspector/binspector.py
+++ library/binspector/binspector.py
@@ -12,6 +12,7 @@ class binspector(ShutItModule):
 	def build(self, shutit):
 		shutit.install('curl')
 		shutit.install('git')
+		shutit.install('libc6')
 		shutit.send('pushd /opt')
 		shutit.send('git clone https://github.com/binspector/binspector.git')
 		shutit.send('pushd /opt')
@@ -26,6 +27,6 @@ def module():
 		'shutit.tk.binspector.binspector', 0.3132235246,
 		description='binary inspector',
 		maintainer='ian.miell@gmail.com',
-		depends=['shutit.tk.setup','shutit.tk.llvm.llvm']
+		depends=['shutit.tk.setup']
 	)
 
