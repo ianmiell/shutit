e3acd516bd7504c752c890d305109fbba5e2297e
Ian Miell <ian.miell@gmail.com>
2014-10-16 19:46:40 +0100

    binspector updated

--- library/binspector/binspector.py
+++ library/binspector/binspector.py
@@ -1,5 +1,4 @@
 """ShutIt module. See http://shutit.tk
-In source, line 16 should be blank, within the build def. This doesn't matter except for test builds, so can be removed once in use.
 """
 
 from shutit_module import ShutItModule
@@ -13,35 +12,17 @@ class binspector(ShutItModule):
 
 
 	def build(self, shutit):
+		shutit.install('curl')
+		shutit.install('git')
 		shutit.send('git clone https://github.com/binspector/binspector.git')
 		shutit.send('./binspector/smoke_test.sh')
 		return True
-
-	#def get_config(self, shutit):
-	#	return True
-
-	#def check_ready(self, shutit):
-	#	return True
-	
-	#def start(self, shutit):
-	#	return True
-
-	#def stop(self, shutit):
-	#    return True
-	#def finalize(self, shutit):
-	#	return True
-
-	#def remove(self, shutit):
-	#	return True
-
-	#def test(self, shutit):
-	#	return True
 
 def module():
 	return binspector(
 		'shutit.tk.binspector.binspector', 0.132235246,
 		description='binary inspector',
 		maintainer='ian.miell@gmail.com',
-		depends=['shutit.tk.setup']
+		depends=['shutit.tk.setup','shutit.tk.llvm.llvm']
 	)
 
