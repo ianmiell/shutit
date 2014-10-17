e2ae60ecd495efce4c25bb0868f29685f175aa3f
Ian Miell <ian.miell@gmail.com>
2014-10-17 08:38:29 +0100

    try

--- library/binspector/binspector.py
+++ library/binspector/binspector.py
@@ -14,11 +14,17 @@ class binspector(ShutItModule):
 		shutit.install('git')
 		shutit.install('python2.7-minimal')
 		shutit.install('libc6')
-		shutit.send('pushd /opt')
+		# TODO: Make exports part of llvm
+		shutit.send('export LIBRARY_PATH=:/usr/lib/x86_64-linux-gnu')
+		shutit.send('export CPATH=/usr/include/c++/4.8:/usr/include/x86_64-linux-gnu/c++/4.8')
+		# Hack to make clang use libstdc++ rather than c++
+		shutit.send('ln -s /usr/lib/gcc/x86_64-linux-gnu/4.8/libstdc++.so /usr/lib/gcc/x86_64-linux-gnu/4.8/libc++.so')
+
+		shuti.tsend('pushd /opt')
 		shutit.send('git clone https://github.com/binspector/binspector.git')
 		shutit.send('popd')
 		shutit.send('pushd /opt/binspector')
-		shutit.pause_point('CPATH')
+
 		shutit.send('./configure.sh')
 		shutit.send('./build.sh')
 		shutit.send('./smoke_test.sh')
