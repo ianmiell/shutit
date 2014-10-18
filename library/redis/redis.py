221bc85b8cc3382e7958de30a5bbef10b2919a44
Ian Miell <ian.miell@gmail.com>
2014-10-18 12:51:09 +0100

    popd

--- library/redis/redis.py
+++ library/redis/redis.py
@@ -15,7 +15,6 @@ class redis(ShutItModule):
 		shutit.send('pushd /tmp')
 		shutit.send('wget http://download.redis.io/redis-stable.tar.gz')
 		shutit.send('tar xvzf redis-stable.tar.gz')
-		shutit.send('popd')
 		shutit.send('pushd redis-stable')
 		shutit.send('make')
 		shutit.send('make install')
@@ -27,6 +26,7 @@ class redis(ShutItModule):
 		shutit.send("""sed -i 's/^\(daemonize .*\)$/# \1/' /etc/redis/redis.conf""")
 		shutit.send("""sed -i 's/^\(dir .*\)$/# \1\ndir \/data/' /etc/redis/redis.conf""")
 		shutit.send("""sed -i 's/^\(logfile .*\)$/# \1/' /etc/redis/redis.conf""")
+		shutit.send('popd')
 		shutit.send('popd')
 		return True
 
