91a5b9cf79efc7de76f50a1583751b7e5d11fdd4
Ian Miell <ian.miell@gmail.com>
2014-10-23 19:41:19 +0100

    notest

--- library/hadoop/hadoop.py
+++ library/hadoop/hadoop.py
@@ -20,6 +20,21 @@ class hadoop(ShutItModule):
 		shutit.send('wget http://mirror.gopotato.co.uk/apache/hadoop/common/stable/hadoop-2.5.1.tar.gz')
 		shutit.send('tar -zxvf hadoop*tar.gz')
 		shutit.pause_point('install hadoop')
+#"Unpack the downloaded Hadoop distribution. In the distribution, edit the file etc/hadoop/hadoop-env.sh to define some parameters as follows:
+#
+#  # set to the root of your Java installation
+#  export JAVA_HOME=/usr/java/latest
+#
+#  # Assuming your installation directory is /usr/local/hadoop
+#  export HADOOP_PREFIX=/usr/local/hadoop
+#
+#Try the following command:
+#
+#  $ bin/hadoop
+#
+#This will display the usage documentation for the hadoop script.
+#
+#Now you are ready to start your Hadoop cluster in one of the three supported modes:"
 		shutit.send('popd /opt')
 		return True
 
