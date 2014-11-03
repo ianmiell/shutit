51c57925a89801d507ce916b1b53df947046c855
Ian Miell <ian.miell@openbet.com>
2014-11-03 10:50:47 +0000

    failed attempt to improve prompt, abandoning

--- shutit_global.py
+++ shutit_global.py
@@ -1304,10 +1304,10 @@ class ShutIt(object):
 
 
 	def setup_prompt(self,
-					 prompt_name,
-					 prefix='TMP',
-					 child=None,
-					 set_default_expect=True):
+	                 prompt_name,
+	                 prefix='TMP',
+	                 child=None,
+	                 set_default_expect=True):
 		"""Use this when you've opened a new shell to set the PS1 to something
 		sane. By default, it sets up the default expect so you don't have to
 		worry about it and can just call shutit.send('a command').
@@ -1346,9 +1346,6 @@ class ShutIt(object):
 			shutit.log('Resetting default expect to: ' +
 			shutit.cfg['expect_prompts'][prompt_name])
 			self.set_default_expect(shutit.cfg['expect_prompts'][prompt_name])
-		# Now we've set up the prompt, we don't need the newline at the front.
-		# This helps us when the returned output does not include a newline at the end.
-		shutit.cfg['expect_prompts'][prompt_name] = local_prompt
 
 
 	def revert_prompt(self, old_prompt_name, new_expect=None, child=None):
