d08fbeef13f36f29bc64a1923e2d17725de33ad4
Ian Miell <ian.miell@gmail.com>
2014-11-11 22:25:42 +0000

    jq

---
+++ library/jq/jq.py
@@ -0,0 +1,52 @@
+"""ShutIt module. See http://shutit.tk
+"""
+
+from shutit_module import ShutItModule
+
+
+class jq(ShutItModule):
+
+
+	def is_installed(self, shutit):
+		return False
+
+
+	def build(self, shutit):
+		shutit.install('flex')
+		shutit.install('bison')
+		shutit.send('autoreconf -i')
+		shutit.send('./configure')
+		shutit.send('make -j8')
+		shutit.send('make check')
+		shutit.send('make install')
+		return True
+
+	#def get_config(self, shutit):
+	#	return True
+
+	#def check_ready(self, shutit):
+	#	return True
+	
+	#def start(self, shutit):
+	#	return True
+
+	#def stop(self, shutit):
+	#	return True
+
+	#def finalize(self, shutit):
+	#	return True
+
+	#def remove(self, shutit):
+	#	return True
+
+	#def test(self, shutit):
+	#	return True
+
+def module():
+	return jq(
+		'shutit.jq.jq.jq', 126456975.00,
+		description='',
+		maintainer='',
+		depends=['shutit.tk.setup']
+	)
+
