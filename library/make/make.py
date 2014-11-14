ea2069e03a11fb4517c723f246b979f01e2de62f
Ian Miell <ian.miell@gmail.com>
2014-11-14 21:36:43 +0000

    oops

--- library/make/make.py
+++ library/make/make.py
@@ -8,25 +8,27 @@ class make(ShutItModule):
 
 
 	def is_installed(self, shutit):
+		return False
+
+
+	def build(self, shutit):
 		shutit.install('wget')
 		shutit.install('gcc')
 		shutit.install('bzip2')
 		shutit.send('pushd /opt')
+		shutit.send('mkdir -p make')
+		shutit.send('pushd /opt/make')
 		shutit.send('wget http://ftp.gnu.org/gnu/make/make-4.1.tar.bz2')
 		shutit.send('bunzip2 make-4.1.tar.bz2')
 		shutit.send('tar -xvf make-4.1.tar')
 		shutit.send('pushd make-4.1')
 		shutit.send('./configure')
-		shutit.send('sh build.sh') # to install make without make
-		shutit.send('./make install') # to install make without make
+		shutit.send('sh build.sh') # to build make without make
+		shutit.send('./make install')
 		shutit.send('popd')
 		shutit.send('popd')
-		shutit.send('rm -rf /opt/make-*')
-		return False
-
-
-	def build(self, shutit):
-
+		shutit.send('popd')
+		shutit.send('rm -rf /opt/make')
 		return True
 
 	#def get_config(self, shutit):
