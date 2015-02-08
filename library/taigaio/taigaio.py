b2f652665235f14dbc0475eb35a5c69c00300a4d
Ian Miell <ian.miell@gmail.com>
2015-02-08 18:55:09 +0000

    cd

--- library/taigaio/taigaio.py
+++ library/taigaio/taigaio.py
@@ -23,6 +23,7 @@ class taigaio(ShutItModule):
 		shutit.send('echo taiga:taiga | chpasswd')
 		shutit.send('adduser taiga sudo')
 		shutit.login(user='taiga')
+		shutit.send('cd /home/taiga')
 		shutit.send('git clone https://github.com/taigaio/taiga-scripts.git')
 		shutit.send('pushd taiga-scripts')
 		shutit.multisend('bash setup-server.sh', {
