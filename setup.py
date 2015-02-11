c566420bcda82174b2b2223d0033c822f21b6bdd
Ian Miell <ian.miell@gmail.com>
2015-02-11 11:55:50 +0000

    speed up build

--- setup.py
+++ setup.py
@@ -91,14 +91,14 @@ class ShutItConnModule(ShutItModule):
 		# Create the build directory and put the config in it.
 		shutit.send('mkdir -p ' + cfg['build']['build_db_dir'] + \
 			 '/' + cfg['build']['build_id'])
-		# Record the command we ran and the python env.
-		# wait for "docker debug" command
-		shutit.send_file(cfg['build']['build_db_dir'] + '/' + \
-		    cfg['build']['build_id'] + '/python_env.sh', \
-		    str(sys.__dict__), log=False)
-		shutit.send_file(cfg['build']['build_db_dir'] + '/' + \
-		    cfg['build']['build_id'] + '/command.sh', \
-		    ' '.join(command), log=False)
+		# Record the command we ran and the python env if in debug.
+		if shutit.cfg['build']['debug']:
+			shutit.send_file(cfg['build']['build_db_dir'] + '/' + \
+			    cfg['build']['build_id'] + '/python_env.sh', \
+			    str(sys.__dict__), log=False)
+			shutit.send_file(cfg['build']['build_db_dir'] + '/' + \
+			    cfg['build']['build_id'] + '/command.sh', \
+			    ' '.join(command), log=False)
 		shutit.pause_point('Anything you want to do now the ' +
 		    'target is connected to?', level=2)
 
