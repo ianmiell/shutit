3c63cbdd64a2784f8c8b43701aa5d7a304b1c812
Ian Miell <ian.miell@gmail.com>
2015-02-12 09:19:29 +0000

    comment removed - line 11 must be blank

--- assets/shutit_module_template_bare.py
+++ assets/shutit_module_template_bare.py
@@ -1,11 +1,11 @@
 """ShutIt module. See http://shutit.tk
-In source, line 11 should be blank, within the build def. This doesn't matter except for test builds, so can be removed once in use.
 """
 
 from shutit_module import ShutItModule
 
 
 class template(ShutItModule):
+
 
 	def build(self, shutit):
 
