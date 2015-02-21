512146b89517b6c395f4178a99b843222a4d9835
Ian Miell <ian.miell@gmail.com>
2015-02-21 10:32:34 +0000

    supervisor

--- library/supervisor/supervisor.py
+++ library/supervisor/supervisor.py
@@ -7,7 +7,6 @@ class supervisor(ShutItModule):
 	def build(self, shutit):
 		shutit.send('echo "deb http://archive.ubuntu.com/ubuntu precise main universe" > /etc/apt/sources.list')
 		shutit.send('apt-get update')
-		shutit.send('apt-get upgrade -y')
 		shutit.install('supervisor')
 		shutit.send('mkdir -p /var/log/supervisor')
 		shutit.send_host_file('/etc/supervisor/conf.d/supervisord.conf', 'context/supervisord.conf')
