d9de0ca59e8132f1ae3e286666c58c6705da66af
Ian Miell <ian.miell@gmail.com>
2015-02-24 18:31:52 +0000

    mariadb removed

--- library/mariadb/bin/run.sh
+++
@@ -1,2 +0,0 @@
-# Example for running
-docker run -t -i -p 3306:3306 mariadb  /bin/bash /start.sh
--- library/mariadb/context/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-__run_supervisor() {
-echo "Running the run_supervisor function."
-supervisord -n
-}
-
-# Call all functions
-__run_supervisor
