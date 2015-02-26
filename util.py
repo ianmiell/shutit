a0c29bc387a288ef195468d108d97d1896900625
Marcello Romani <marcello.romani@libero.it>
2015-02-26 16:31:28 +0000

    Output build log to file by default. Print location of log file at the end of the build.

--- util.py
+++ util.py
@@ -150,7 +150,7 @@ ssh_cmd:
 
 # Aspects of build process
 [build]
-build_log:no
+build_log:yes
 # How to connect to target
 conn_module:shutit.tk.conn_docker
 # Run any docker container in privileged mode
@@ -363,6 +363,7 @@ def get_base_config(cfg, cfg_parser):
 		logfile = os.path.join('/tmp/', 'shutit_log_' + cfg['build']['build_id'])
 	else:
 		logfile = logfile + '_' + cfg['build']['build_id']
+	cfg['host']['logfile'] = logfile
 	if cfg['build']['build_log']:
 		cfg['build']['build_log'] = open(logfile, 'a')
 		# Lock it down to the running user.
