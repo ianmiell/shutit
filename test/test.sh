bc0f5a51eb3e6031dff3deeaaf608fc39a138a87
Ian Miell <ian.miell@gmail.com>
2015-05-23 14:39:22 +0100

    tests

--- test/test.sh
+++ test/test.sh
@@ -60,7 +60,7 @@ cleanup hard
 
 DESC="Testing skeleton build with Dockerfile"
 echo $DESC
-./shutit skeleton -d assets/dockerfile/Dockerfile ${NEWDIR} testing shutit.tk --depends shutit.tk.setup --base_image ubuntu:14.04
+./shutit skeleton -d assets/dockerfile/Dockerfile --module_directory ${NEWDIR} --module_name testing --domain shutit.tk --depends shutit.tk.setup --base_image ubuntu:14.04
 pushd ${NEWDIR}/bin
 ./test.sh
 if [[ "x$?" != "x0" ]]
@@ -75,7 +75,7 @@ popd > /dev/null 2>&1
 
 DESC="Testing skeleton build basic bare"
 echo $DESC
-./shutit skeleton ${NEWDIR} testing shutit.tk --depends shutit.tk.setup --base_image ubuntu:14.04
+./shutit skeleton --module_directory ${NEWDIR} --module_name testing --domain shutit.tk --depends shutit.tk.setup --base_image ubuntu:14.04
 pushd ${NEWDIR}/bin
 ./test.sh
 if [[ "x$?" != "x0" ]]
@@ -91,7 +91,7 @@ popd > /dev/null 2>&1
 
 DESC="Testing skeleton build basic with example script"
 echo $DESC
-./shutit skeleton ${NEWDIR} testing shutit.tk --depends shutit.tk.setup --base_image ubuntu:14.04 --script ${SHUTIT_DIR}/assets/example.sh
+./shutit skeleton --module_directory ${NEWDIR} --module_name testing --domain shutit.tk --depends shutit.tk.setup --base_image ubuntu:14.04 --script ${SHUTIT_DIR}/assets/example.sh
 
 pushd ${NEWDIR}/bin
 ./test.sh
