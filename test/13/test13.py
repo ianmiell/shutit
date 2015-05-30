64304e4202ce4d715f714a531d76863151f0e38f
Ian Miell <ian.miell@gmail.com>
2015-05-30 16:07:23 +0100

    test 13 added

---
+++ test/13/test13.py
@@ -0,0 +1,41 @@
+"""ShutIt module. See http://shutit.tk
+"""
+
+from shutit_module import ShutItModule
+
+
+class test13(ShutItModule):
+
+
+	def build(self, shutit):
+		a = '''a
+b
+c
+d
+e'''
+		shutit.send_file('/tmp/a',a)
+
+		res1 = shutit.send_and_get_output('md5sum /tmp/a')
+		shutit.insert_text(r'''b
+c''','/tmp/a','a')
+		res2 = shutit.send_and_get_output('md5sum /tmp/a')
+		if res1 != res2:
+			shutit.fail('file was altered')
+
+		res1 = shutit.send_and_get_output('md5sum /tmp/a')
+		shutit.insert_text('''c
+c''','/tmp/a','a')
+		res2 = shutit.send_and_get_output('md5sum /tmp/a')
+		if res1 == res2:
+			shutit.fail('file not altered')
+
+		return True
+
+def module():
+	return test13(
+		'shutit.tk.test13.test13', 782914092.00,
+		description='',
+		maintainer='',
+		depends=['shutit.tk.setup']
+	)
+
