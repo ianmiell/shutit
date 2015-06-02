afb00343e7fefb3dc56e69df6d09809c2d566b6a
Ian Miell <ian.miell@gmail.com>
2015-06-02 18:10:29 +0100

    escape

--- shutit_global.py
+++ shutit_global.py
@@ -919,6 +919,8 @@ class ShutIt(object):
 							created_file = True
 						#            v the space is intentional, to avoid polluting bash history.
 						replaced_line = line.replace("'","""'"'"'""")
+						replaced_line = line.replace("[",r"\[")
+						replaced_line = line.replace("]",r"\]")
 						self.send(""" grep -w '^""" + 
 								  replaced_line +
 								  """$' """ +
