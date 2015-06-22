9c453b2150f4e07eba371b16ccafad0bd491511c
Ian Miell <ian.miell@gmail.com>
2015-06-22 18:39:09 +0100

    Function to prompt for input done. Password handled.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
 	# Versions should comply with PEP440.  For a discussion on single-sourcing
 	# the version across setup.py and the project code, see
 	# https://packaging.python.org/en/latest/single_source_version.html
-	version='0.8.34',
+	version='0.8.35',
 
 	description='An automation tool designed for complex Docker builds',
 	long_description=long_description,
