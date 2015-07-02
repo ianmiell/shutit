8242e7f5dedb32471409c6e2f64f22f98bce406a
Ian Miell <ian.miell@gmail.com>
2015-07-02 22:57:41 +0100

    fixed head error

--- shutit_global.py
+++ shutit_global.py
@@ -592,7 +592,7 @@ $'"""
 		"""Internal helper function to get command that's really sent"""
 		cmd_arr = send.split()
 		if len(cmd_arr) and cmd_arr[0] in ('md5sum','sed','head'):
-			send = string.join([self._get_command(cmd_arr[0])] + cmd_arr[1:])
+			send = string.join(self._get_command(cmd_arr[0]) + send[len(self._get_command(cmd_arr[0])):],'')
 		return send
 
 	def _handle_note(self, note):
@@ -658,7 +658,7 @@ $'"""
 			if cfg['environment'][cfg['build']['current_environment_id']]['distro'] == 'osx':
 				return '''PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH" ''' + command + ' '
 			else:
-				return 'head '
+				return command + ' '
 		return command
 
 
