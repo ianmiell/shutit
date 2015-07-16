c429cb7c42ce567769ae347edd16f4e5a7404e0b
Ian Miell <ian.miell@gmail.com>
2015-07-16 08:41:25 +0100

    71

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ setup(
 	# Versions should comply with PEP440.  For a discussion on single-sourcing
 	# the version across setup.py and the project code, see
 	# https://packaging.python.org/en/latest/single_source_version.html
-	version='0.8.70',
 
+	version='0.8.71',
 	description='An automation tool designed for complex Docker builds',
 	long_description=long_description,
 
