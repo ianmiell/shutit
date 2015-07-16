4cb477bece2ccea93ac451725300316ddf24b297
Ian Miell <ian.miell@gmail.com>
2015-07-16 15:38:30 +0100

    virtualbox

--- package_map.py
+++ package_map.py
@@ -34,6 +34,7 @@ PACKAGE_MAP = {
 	'ruby-dev':              {'apt':'ruby-dev',          'yum':'ruby-devel',    'brew':'ruby-build'},
 	'git':                   {'emerge':'dev-vcs/git'},
 	'vagrant':               {'brew':'Caskroom/cask/vagrant'},
+	'virtualbox':            {'brew':'Caskroom/cask/virtualbox'},
 	'build-essential':       {'brew':'gcc'},
 }
 
