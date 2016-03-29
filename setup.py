47a17bb118dc68b2c0017fcadcea196220fd1822
ianmiell <ian.miell@gmail.com>
2016-03-29 19:05:23 +0100

    0.9

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
 	# the version across setup.py and the project code, see
 	# https://packaging.python.org/en/latest/single_source_version.html
 
-	version='0.8.160',
+	version='0.9.0',
 	description='An automation tool designed for complex Docker builds',
 	long_description=long_description,
 
