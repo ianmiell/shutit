9d85c86b65236bdfaa10fa6da10d535a71cf308f
ianmiell <ian.miell@gmail.com>
2016-04-28 10:26:13 +0100

    release

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # pip release scripts that auto-updates version number and keeps trying until successful
-#set -x
+set -x
 set -u
 i=0
 while true
@@ -12,8 +12,8 @@ do
 	point=$(echo $output | awk '{print $3}')
 	newpoint=$[point+1]
 	sed -i "s/\([ \s]\)*version=\(.\)$major.$minor.$point\(.\).*/\1version=\2$major.$minor.$newpoint\3,/" setup.py
-	sed -i "s/^shutit_version=\(.\)$major.$minor.$point\(.\).*/shutit_version=\1$major.$minor.$newpoint\2/" shutit_main.py
-	python setup.py sdist  bdist_wheel upload 
+	sed -i "s/^shutit_version=\(.\)$major.$minor.[0-9][0-9]*\(.\).*/shutit_version=\1$major.$minor.$newpoint\2/" shutit_main.py
+	#python setup.py sdist bdist_wheel upload 
 	if [[ $? = 0 ]]
 	then
 		break
