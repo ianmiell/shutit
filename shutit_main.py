5eb1a3e09e4253a8e0f194415a1153582a7e1590
Ian Miell <ian.miell@gmail.com>
2016-05-21 16:24:30 +0100

    level

--- shutit_main.py
+++ shutit_main.py
@@ -750,7 +750,7 @@ def setup_signals():
 	signal.signal(signal.SIGINT, shutit_util.ctrl_c_signal_handler)
 	signal.signal(signal.SIGQUIT, shutit_util.ctrl_quit_signal_handler)
 
-shutit_version='0.9.154'
+shutit_version='0.9.155'
 if __name__ == '__main__':
 	setup_signals()
 	main()
--- shutit_pexpect.py
+++ shutit_pexpect.py
@@ -553,7 +553,7 @@ class ShutItPexpectSession(object):
 					fixterm_filename = '/tmp/shutit_fixterm'
 					fixterm_filename_stty = fixterm_filename + '_stty'
 					if not self.file_exists(fixterm_filename):
-						shutit.log('Fixing up your terminal, please wait...',loglevel=logging.INFO)
+						shutit.log('Fixing up your terminal, please wait...',level=logging.INFO)
 						self.send_file(fixterm_filename,shutit_assets.get_fixterm(), loglevel=logging.DEBUG, delaybeforesend=delaybeforesend)
 						self.send(' chmod 777 ' + fixterm_filename, echo=False,loglevel=logging.DEBUG, delaybeforesend=delaybeforesend)
 					if not self.file_exists(fixterm_filename + '_stty'):
