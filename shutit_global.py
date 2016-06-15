885ecc2d78f845cc0ff43399653f491026d74111
Ian Miell <ian.miell@gmail.com>
2016-06-15 21:31:43 +0100

    send_and_return_status

--- shutit_global.py
+++ shutit_global.py
@@ -418,8 +418,30 @@ class ShutIt(object):
 	# alias send to send_and_expect
 	send_and_expect = send
 
-	
 
+	# Returns true if a good exit code was received (usually 0)
+	def send_and_return_status(send,
+	                           expect=None,
+	                           shutit_pexpect_child=None,
+	                           timeout=None,
+	                           check_exit=None,
+	                           fail_on_empty_before=True,
+	                           record_command=True,
+	                           exit_values=None,
+	                           echo=None,
+	                           escape=False,
+	                           retry=3,
+	                           note=None,
+	                           assume_gnu=True,
+	                           follow_on_commands={},
+	                           delaybeforesend=0,
+	                           loglevel=logging.INFO):
+		shutit_pexpect_child = shutit_pexpect_child or self.get_current_shutit_pexpect_session().pexpect_child
+		shutit_pexpect_session = self.get_shutit_pexpect_session_from_child(shutit_pexpect_child)
+		shutit_pexpect_session.send(send,expect=expect,timeout=timeout,check_exit=check_exit,fail_on_empty_before=fail_on_empty_before,record_command=record_command,exit_values=exit_values,echo=echo,escape=escape,retry=retry,note=note,assume_gnu=assume_gnu,delaybeforesend=delaybeforesend,loglevel=loglevel,follow_on_commands=follow_on_commands)
+		return shutit.check_last_exit_values(send,expect=expect,exit_values=exit_values,retry=retry,retbool=True)
+
+                                                                                                                  
 	def _handle_note(self, note, command='', training_input=''):
 		"""Handle notes and walkthrough option.
 
