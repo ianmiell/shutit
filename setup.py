72c723dd25f1c43036de8e4235f5b00213a870e2
Ian Miell <ian.miell@gmail.com>
2016-10-20 09:24:29 +0100

    gitignore

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
 	# need to place data files outside of your packages. See:
 	# http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
 	# In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-	data_files=[('.gitignore')],
+	data_files=[],
 
 	# To provide executable scripts, use entry points in preference to the
 	# "scripts" keyword. Entry points provide cross-platform support and allow
