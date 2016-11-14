5e01851db536ef81004f1690d2ad35c7b13f720f
Ian Miell <ian.miell@gmail.com>
2016-11-14 09:13:23 +0000

    addecho

--- shutit_patterns/vagrant_multinode.py
+++ shutit_patterns/vagrant_multinode.py
@@ -30,7 +30,7 @@ then
 	echo "Must have shutit on path, eg export PATH=$PATH:/path/to/shutit_dir"
 	exit 1
 fi
-$SHUTIT build -d bash -m shutit-library/vagrant -m shutit-library/virtualbox "$@"
+$SHUTIT build --echo -d bash -m shutit-library/vagrant -m shutit-library/virtualbox "$@"
 if [[ $? != 0 ]]
 then
 	exit 1
