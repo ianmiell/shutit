8786671bd4f8739f0a86ce27c6de305f25aac174
Ian Miell <ian.miell@gmail.com>
2017-05-03 07:30:52 +0100

    multisend

--- shutit_pexpect.py
+++ shutit_pexpect.py
@@ -1904,6 +1904,7 @@ class ShutItPexpectSession(object):
 		expect_list = list(send_dict)
 		# Put breakout item(s) in last.
 		n_breakout_items = 0
+		shutit.log('In multisend, send: ' + send + ' send_dict: ' + str(send_dict),level=logging.INFO)
 		if isinstance(expect, str):
 			shutit.log('Adding: "' + expect + '" to expect list.',level=logging.DEBUG)
 			expect_list.append(expect)
