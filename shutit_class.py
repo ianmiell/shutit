c3a4f9cc4485c422db92ccf4ba66dc211a6a8db4
Ian Miell <ian.miell@gmail.com>
2017-05-21 13:03:23 +0100

    LCP

--- shutit_class.py
+++ shutit_class.py
@@ -42,6 +42,75 @@ from shutit_pexpect import ShutItPexpectSession
 
 PY3 = (sys.version_info[0] >= 3)
 
+
+class LayerConfigParser(ConfigParser.RawConfigParser):
+
+	def __init__(self):
+		ConfigParser.RawConfigParser.__init__(self)
+		self.layers = []
+
+	def read(self, filenames):
+		if not isinstance(filenames, list):
+			filenames = [filenames]
+		for filename in filenames:
+			cp = ConfigParser.RawConfigParser()
+			cp.read(filename)
+			self.layers.append((cp, filename, None))
+		return ConfigParser.RawConfigParser.read(self, filenames)
+
+	def readfp(self, fp, filename=None):
+		cp = ConfigParser.RawConfigParser()
+		fp.seek(0)
+		cp.readfp(fp, filename)
+		self.layers.append((cp, filename, fp))
+		fp.seek(0)
+		ret = ConfigParser.RawConfigParser.readfp(self, fp, filename)
+		return ret
+
+	def whereset(self, section, option):
+		for cp, filename, fp in reversed(self.layers):
+			fp = fp # pylint
+			if cp.has_option(section, option):
+				return filename
+		raise ShutItFailException('[%s]/%s was never set' % (section, option)) # pragma: no cover
+
+	def get_config_set(self, section, option):
+		"""Returns a set with each value per config file in it.
+		"""
+		values = set()
+		for cp, filename, fp in self.layers:
+			filename = filename # pylint
+			fp = fp # pylint
+			if cp.has_option(section, option):
+				values.add(cp.get(section, option))
+		return values
+
+	def reload(self):
+		"""
+		Re-reads all layers again. In theory this should overwrite all the old
+		values with any newer ones.
+		It assumes we never delete a config item before reload.
+		"""
+		oldlayers = self.layers
+		self.layers = []
+		for cp, filename, fp in oldlayers:
+			cp = cp # pylint
+			if fp is None:
+				self.read(filename)
+			else:
+				self.readfp(fp, filename)
+
+	def remove_section(self, *args, **kwargs):
+		raise NotImplementedError('''Layer config parsers aren't directly mutable''') # pragma: no cover
+
+	def remove_option(self, *args, **kwargs):
+		raise NotImplementedError('''Layer config parsers aren't directly mutable''') # pragma: no cover
+
+	def set(self, *args, **kwargs):
+		raise NotImplementedError('''Layer config parsers aren\'t directly mutable''') # pragma: no cover
+
+
+
 class ShutItInit(object):
 	"""Object used to initialise a shutit object.
 	"""
