72113a688d24f7ba70d449d90c32649b75f2fb2e
Ian Miell <ian.miell@gmail.com>
2017-05-24 19:55:47 +0100

    block other commands

--- shutit_class.py
+++ shutit_class.py
@@ -640,6 +640,7 @@ class ShutIt(object):
 	         nonewline=False,
 	         background=False,
 	         wait=True,
+	         block=True,
 	         loglevel=logging.INFO):
 		"""Send string as a shell command, and wait until the expected output
 		is seen (either a string or any from a list of strings) before
@@ -663,6 +664,7 @@ class ShutIt(object):
 		@param note: See shutit.ShutItSendSpec
 		@param assume_gnu: See shutit.ShutItSendSpec
 		@param wait: See shutit.ShutItSendSpec
+		@param block: See shutit.ShutItSendSpec.block_other_commands
 		@return: The pexpect return value (ie which expected string in the list matched)
 		@rtype: string
 		"""
@@ -691,7 +693,8 @@ class ShutIt(object):
 		                                                  secret=secret,
 		                                                  nonewline=nonewline,
 		                                                  run_in_background=background,
-		                                                  ignore_background=ignore_background))
+		                                                  ignore_background=ignore_background,
+		                                                  block_other_commands=block))
 
 
 	def send_and_return_status(self,
