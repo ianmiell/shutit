850137aa838c5cc5fb64f37b06ae112014a7c90c
Ian Miell <ian.miell@gmail.com>
2018-03-31 19:05:43 +0100

    release

--- release.sh
+++ release.sh
@@ -14,7 +14,7 @@ do
 	datestr=$(date)
 	newpoint=$[point+1]
 	sed -i "s/\([ \s]\)*version=\(.\)$major.$minor.$point\(.\).*/\1version=\2$major.$minor.$newpoint\3,/" setup.py
-	sed -i "s/^shutit_version=\(.\)$major.$minor.[0-9][0-9]*\(.\).*/shutit_version=\1$major.$minor.$newpoint\2, uploaded at $datestr/" shutit.py
+	sed -i "s/^shutit_version=\(.\)$major.$minor.[0-9][0-9]*\(.\).*/shutit_version=\1$major.$minor.$newpoint\2" shutit.py
 	python setup.py sdist bdist_wheel upload 
 	if [[ $? = 0 ]]
 	then
