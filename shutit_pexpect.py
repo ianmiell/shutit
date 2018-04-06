05bb7acfe1c6f6f94ae3e2e7c62c80f54f85c26c
Ian Miell <ian.miell@gmail.com>
2018-04-06 09:08:09 +0100

    even simpler

--- shutit_pexpect.py
+++ shutit_pexpect.py
@@ -378,12 +378,11 @@ class ShutItPexpectSession(object):
 			self.setup_prompt(r_id,capture_exit_code=True)
 		self.login_stack.append(r_id)
 		shutit_global.shutit_global_object.log('Login stack after login: ' + str(self.login_stack),level=logging.DEBUG)
-		login_exit = self.send_and_get_output(''' echo $SHUTIT_EC && unset SHUTIT_EC''')
-		if login_exit != '0':
+		if self.send_and_get_output(''' echo $SHUTIT_EC && unset SHUTIT_EC''') != '0':
+			# TODO: remove just-added login stack item (since we failed to log in successfully)?
 			if sendspec.fail_on_fail: # pragma: no cover
 				self.shutit.fail('Login failure!')
 			else:
-				# TODO: remove just-added login stack item (since we failed to log in successfully)?
 				return False
 		if sendspec.go_home:
 			self.send(ShutItSendSpec(self,
