8d0ba26361648254048582b18fe51c2382649fe8
Ian Miell <ian.miell@gmail.com>
2018-05-28 12:52:24 +0100

    moved

--- shutit_global.py
+++ shutit_global.py
@@ -332,6 +332,101 @@ class ShutItGlobal(object):
 		sys.exit(exit_code)
 
 
+
+
+# shutit_global.shutit_objects have the pexpect sessions in their shutit_pexpect_sessions variable.
+class PaneManager(object):
+	only_one = None
+	def __init__(self):
+		"""
+		
+		only_one             - singleton insurance
+		"""
+		assert self.only_one is None
+		self.only_one is True
+		# TODO: screen width and height
+		# Keep it simple for now by creating four panes
+		top_left     = SessionPane('top_left')
+		top_right    = SessionPane('top_right')
+		bottom_left  = SessionPane('bottom_left')
+		bottom_right = SessionPane('bottom_right')
+		self.window     = None
+		self.screen_arr = None
+		self.wheight    = None
+		self.wwidth     = None
+		self.refresh_window()
+	# TODO: place panes in appropriate locations.
+	# TODO: logs directed to one pane. https://stackoverflow.com/questions/31999627/storing-logger-messages-in-a-string - log to stringio and then clear
+	# TODO: send/expect to another
+	# TODO: code context to another
+
+
+	def refresh_window(self):
+		self.window               = curtsies.FullscreenWindow(hide_cursor=True)
+		self.screen_arr           = None
+		self.wheight              = self.window.height
+		self.wwidth               = self.window.width
+		# Divide the screen up into two, to keep it simple for now
+		self.wheight_top_end      = int(self.wheight / 2)
+		self.wheight_bottom_start = int(self.wheight / 2)
+		self.wwidth_left_end      = int(self.wwidth / 2)
+		self.wwidth_right_start   = int(self.wwidth / 2)
+		assert self.wheight >= 24, self.quit_autotrace('Terminal not tall enough!')
+		assert self.wwidth >= 80, self.quit_autotrace('Terminal not wide enough!')
+
+
+	def draw_screen(self, clear_screen=False, quick_help='HELP TODO'):
+		assert draw_type in ('sessions','help','clearscreen')
+		self.screen_arr = curtsies.FSArray(self.wheight, self.wwidth)
+		# Header
+		header_text = 'Shutit'
+		self.screen_arr[0:1,0:len(header_text)] = [blue(header_text)]
+		# Footer
+		space = (self.wwidth - len(quick_help))*' '
+		footer_text = space + quick_help
+		self.screen_arr[self.wheight-1:self.wheight,0:len(footer_text)] = [invert(blue(footer_text))]
+		# Draw the sessions.
+		# Is there a zoomed session? Just write that one out.
+		# TODO: get sessions and write write_out_session_to_fit_pane function
+		if clear_screen:
+			for y in range(0,self.wheight):
+				line = ' '*self.wwidth
+				self.screen_arr[y:y+1,0:len(line)] = [line]
+		for session in self.pexpect_sessions:
+			session.write_out_session_to_fit_pane()
+
+
+# Represents a window pane with no concept of context or content.
+class SessionPane(object):
+	def __init__(self, name):
+		self.name                 = name
+		self.top_left_x           = -1
+		self.top_left_y           = -1
+		self.bottom_right_x       = -1
+		self.bottom_right_y       = -1
+		assert self.name in ('top_left','bottom_left','top_right','bottom_right')
+	def __str__(self):
+		string =  '\n============= SESSION PANE OBJECT BEGIN ==================='
+		string += '\nname: '           + str(self.name)
+		string += '\ntop_left_x: '     + str(self.top_left_x)
+		string += '\ntop_left_y: '     + str(self.top_left_y)
+		string += '\nbottom_right_x: ' + str(self.bottom_right_x)
+		string += '\nbottom_right_y: ' + str(self.bottom_right_y)
+		string += '\nwidth: '          + str(self.get_width())
+		string += '\nheight: '         + str(self.get_width())
+		string += '\n============= SESSION PANE OBJECT END   ==================='
+		return string
+	def set_position(self, top_left_x, top_left_y, bottom_right_x, bottom_right_y):
+		self.top_left_x     = top_left_x
+		self.top_left_y     = top_left_y
+		self.bottom_right_x = bottom_right_x
+		self.bottom_right_y = bottom_right_y
+	def get_width(self):
+		return self.bottom_right_x - self.top_left_x
+	def get_height(self):
+		return self.bottom_right_y - self.top_left_
+
+
 def setup_signals():
 	signal.signal(signal.SIGINT, shutit_util.ctrl_c_signal_handler)
 	signal.signal(signal.SIGQUIT, shutit_util.ctrl_quit_signal_handler)
