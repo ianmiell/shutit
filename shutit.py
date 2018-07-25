3c102ee1a4ebf308332a3239d94ca4a3220ddcdf
Ian Miell <ian.miell@gmail.com>
2018-07-25 13:27:14 +0100

    renames

--- shutit.py
+++ shutit.py
@@ -25,34 +25,33 @@
 
 """ShutIt is a means of building stateless target hosts in a flexible and predictable way.
 """
-import logging
 import sys
 import shutit_global
 import shutit_util
 
 
-def create_session(session_type='bash',
-                   docker_image=None,
-                   vagrant_image='ubuntu/xenial64',
-                   vagrant_provider='virtualbox',
-                   gui=False,
-                   memory='1024',
-                   swapsize='2G',
-                   rm=None,
+def create_session(docker_image=None,
+                   docker_rm=None,
                    echo=False,
-                   walkthrough=False,
+                   loglevel='WARNING',
                    nocolor=False,
-                   video=-1,
+                   session_type='bash',
+                   vagrant_session_name=None,
+                   vagrant_image='ubuntu/xenial64',
+                   vagrant_gui=False,
+                   vagrant_memory='1024',
+                   vagrant_num_machines='1',
+                   vagrant_provider='virtualbox',
+                   vagrant_root_folder=None,
+                   vagrant_swapsize='2G',
                    vagrant_version='1.8.6',
-                   virt_method='virtualbox',
-                   session_name=None,
-                   num_machines='1',
-                   root_folder=None,
-                   loglevel='WARNING'):
+                   vagrant_virt_method='virtualbox',
+                   video=-1,
+                   walkthrough=False):
 	"""Creates a distinct ShutIt session. Sessions can be of type:
 
-	bash    - a bash shell is spawned and 
-	vagrant - a Vagrantfile is created and 'vagrant up'ped 
+	bash    - a bash shell is spawned and
+	vagrant - a Vagrantfile is created and 'vagrant up'ped
 	"""
 	assert session_type in ('bash','docker','vagrant'), shutit_util.print_debug()
 	shutit_global_object = shutit_global.shutit_global_object
@@ -61,35 +60,35 @@ def create_session(session_type='bash',
 	if session_type in ('bash','docker'):
 		return shutit_global_object.create_session(session_type,
 		                                           docker_image=docker_image,
-		                                           rm=rm,
+		                                           rm=docker_rm,
 		                                           echo=echo,
 		                                           walkthrough=walkthrough,
 		                                           walkthrough_wait=video,
 		                                           nocolor=nocolor,
 		                                           loglevel=loglevel)
 	elif session_type == 'vagrant':
-		if session_name is None:
-			session_name = 'shutit_' + shutit_util.random_id()
-		if isinstance(num_machines, int):
-			num_machines = str(num_machines)
-		assert isinstance(num_machines, str)
-		assert isinstance(int(num_machines), int)
-		if root_folder is None:
-			root_folder = shutit_global.shutit_global_object.owd
-		return create_session_vagrant(session_name,
-		                              num_machines,
+		if vagrant_session_name is None:
+			vagrant_session_name = 'shutit_' + shutit_util.random_id()
+		if isinstance(vagrant_num_machines, int):
+			vagrant_num_machines = str(vagrant_num_machines)
+		assert isinstance(vagrant_num_machines, str)
+		assert isinstance(int(vagrant_num_machines), int)
+		if vagrant_root_folder is None:
+			vagrant_root_folder = shutit_global.shutit_global_object.owd
+		return create_session_vagrant(vagrant_session_name,
+		                              vagrant_num_machines,
 		                              vagrant_image,
 		                              vagrant_provider,
-		                              gui,
-		                              memory,
-		                              swapsize,
+		                              vagrant_gui,
+		                              vagrant_memory,
+		                              vagrant_swapsize,
 		                              echo,
 		                              walkthrough,
 		                              nocolor,
 		                              video,
 		                              vagrant_version,
-		                              virt_method,
-		                              root_folder,
+		                              vagrant_virt_method,
+		                              vagrant_root_folder,
 		                              loglevel)
 
 
