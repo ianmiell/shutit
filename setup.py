01221334082efb700a9489238b03f9a6d4ade36b
Ian Miell <ian.miell@gmail.com>
2018-08-11 17:25:10 +0100

    shutit

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ setup(
 	# pip to create the appropriate form of executable for the target platform.
 	entry_points={
 		'console_scripts': [
-			'shutit=shutit_main:main',
+			'shutit=shutit/shutit_main:main',
 		],
 	},
 )
