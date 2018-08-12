d821978def580f5b840dc8699773b8817fc0ee8a
Ian Miell <ian.miell@gmail.com>
2018-08-12 16:38:35 +0100

    shutit_pexpect_session_environment

--- shutit/shutit_pexpect_session_environment.py
+++ shutit/shutit_pexpect_session_environment.py
@@ -1,7 +1,4 @@
 from __future__ import print_function
-import os
-import sys
-sys.path.insert(0,os.path.abspath(os.path.dirname(__file__)) + '/shutit')
 import shutit_util
 
 class ShutItPexpectSessionEnvironment(object):
