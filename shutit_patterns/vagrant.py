8bc632309b53991beb9ef0e8bdd9f16336412438
Ian Miell <ian.miell@gmail.com>
2018-09-20 07:28:21 +0100

    machine_name

--- shutit_patterns/vagrant.py
+++ shutit_patterns/vagrant.py
@@ -201,7 +201,7 @@ If you want to change a config, choose the number: ''')
 			shutit_session.send('vagrant landrush rm ' + machines[machine]['fqdn'])
 			# Needs to be done serially for stability reasons.
 			try:
-				shutit_session.multisend('vagrant up --provider ' + shutit.cfg['shutit-library.virtualization.virtualization.virtualization']['virt_method'] + machine_name,{'assword for':pw,'assword:':pw})
+				shutit_session.multisend('vagrant up --provider ' + shutit.cfg['shutit-library.virtualization.virtualization.virtualization']['virt_method'] + machine,{'assword for':pw,'assword:':pw})
 			except NameError:
 				shutit.multisend('vagrant up ' + machine,{'assword for':pw,'assword:':pw},timeout=99999)
 			if shutit.send_and_get_output("vagrant status 2> /dev/null | grep -w ^" + machine + " | awk '{print $2}'") != 'running':
