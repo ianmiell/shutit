ebefb2879944ab573ec770ef76cd4b9fc7e926e4
Ian Miell <ian.miell@gmail.com>
2018-09-30 13:25:35 +0100

    shellcheck

--- release.sh
+++ release.sh
@@ -6,25 +6,23 @@ i=0
 while true
 do
 	rm -rf build
-	i=$[i+1]
+	i=$((i+1))
 	output=$(grep version= setup.py | awk -F'=' '{print $2}' | sed "s/'\([0-9][0-9]*\)\.\([0-9][0-9]*\)\.\([0-9][0-9]*\)',/\1 \2 \3/")
-	major=$(echo $output | awk '{print $1}')
-	minor=$(echo $output | awk '{print $2}')
-	point=$(echo $output | awk '{print $3}')
-	datestr=$(date)
-	newpoint=$[point+1]
+	major=$(echo "${output}" | awk '{print $1}')
+	minor=$(echo "${output}" | awk '{print $2}')
+	point=$(echo "${output}" | awk '{print $3}')
+	newpoint=$((point+1))
 	sed -i "s/\([ \s]\)*version=\(.\)$major.$minor.$point\(.\).*/\1version=\2$major.$minor.$newpoint\3,/" setup.py
-	sed -i "s/^shutit_version=\(.\)$major.$minor.[0-9][0-9]*\(.\).*/shutit_version=\1$major.$minor.$newpoint\2/" shutit.py
-	python setup.py sdist bdist_wheel upload 
-	if [[ $? = 0 ]]
+	sed -i "s/^shutit_version=\(.\)${major}.${minor}.[0-9][0-9]*\(.\).*/shutit_version=\1${major}.${minor}.${newpoint}\2/" shutit.py
+	if python setup.py sdist bdist_wheel upload 
 	then
 		break
 	fi
 	# wait a minute
 	sleep 60
 done
-git commit -am "release: $major.$minor.$newpoint"
-echo Success after $i attempts
+git commit -am "release: ${major}.${minor}.${newpoint}"
+echo Success after ${i} attempts
 git push
-git tag -f $major.$minor.$point
+git tag -f "${major}.${minor}.${newpoint}"
 git push --tags
